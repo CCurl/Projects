d598b42851a75ede8d6336edd9af72d403b96f90
Chris Curl <curlbud-github.com>
2022-01-30 13:27:12 -0500

    Start of editor

--- R4/editor.cpp
+++ R4/editor.cpp
@@ -2,8 +2,29 @@
 #include "R4.h"
 
 #ifdef __EDITOR__
+void readBlock(int b, addr to) {
+    char fn[24];
+    sprintf(fn, "block-%03d.R4", b);
+    FILE* fp = fopen(fn, "rt");
+    if (fp) {
+        int n = fread(to, 1, USER_SZ, fp);
+        fclose(fp);
+        *(to + n) = 0;
+    } else {
+        *to = 0;
+    }
+}
+
+void GotoXY(int x, int y) { printStringF("%c[%d;%dH", 27, y, x); }
+void CLS() { printStringF("%c[2J", 27); GotoXY(1,1); }
+
 void doEditor(CELL b, addr x) {
     printStringF("editing block %d ... ", b);
+    CLS();
+    readBlock(b, x);
+    for (addr cp = x; *cp; cp++) {
+        printChar(*cp);
+    }
     printString("-TODO-");
 }
 #else
