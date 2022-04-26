f5d5cbf2ed728cd1071505b17e566d38207c8bd0
Chris Curl <curlbud-github@yahoo.com>
2022-04-26 13:12:54 -0400

    Update s2.c

--- S2/s2.c
+++ S2/s2.c
@@ -9,7 +9,7 @@
 #define NOS st.i[s-1]
 union flin { float f[2500]; int i[2500]; char b[10000]; }; static union flin st;
 static char ex[80], t = 0, u, a, k = 0;
-static int x = 0, i, c, v, m, r, p, n, s, f;
+static int x = 0, i, c, v, m, r, b = 8000, p, n, s, f, ro = 64;
 inline void push(int x) { st.i[++s] = x; } 
 inline int pop() { return st.i[s--]; }
 void N() {} // NOP
@@ -29,33 +29,33 @@ void f45() /* - */ { NOS -= TOS; s--; }
 void f46() /* . */ { printf("%d", pop()); }
 void f47() /* / */ { NOS /= TOS; s--; }
 void fN() /* 0-9 */ { push(u-'0'); while (btw(st.b[p],'0','9')) { TOS=(TOS*10)+(st.b[p++]-'0'); } }
-void f58() /* : */ { u=st.b[p++]; st.i[u]=p; while (st.b[p++]!=';') {} }
-void f59() /* ; */ { p=st.i[r--]; }
-void f60() /* < */ { NOS=(NOS<TOS)?1:0; s--; }
-void f61() /* = */ { NOS=(NOS==TOS)?1:0; s--; }
-void f62() /* > */ { NOS=(NOS>TOS)?1:0; s--; }
+void f58() /* : */ { u = st.b[p++]; if (btw(u,'A','Z')) { st.i[u] = p; while (st.b[p++] != ';') {} m = p; } }
+void f59() /* ; */ { p = st.i[r--]; }
+void f60() /* < */ { NOS = (NOS < TOS) ? 1 : 0; s--; }
+void f61() /* = */ { NOS = (NOS == TOS) ? 1 : 0; s--; }
+void f62() /* > */ { NOS = (NOS > TOS) ? 1 : 0; s--; }
 void f63() /* ? */ {}
 void f64() /* @ */ { TOS = st.i[TOS]; }
-void fF() /* A-Z */ { st.i[++r]=p; p=st.i[u]; }
-void f91() /* [ */ { st.i[++r]=p; st.i[++r]=pop(); st.i[++r]=pop(); }
+void fF() /* A-Z */ { st.i[++r] = p; p = st.i[u]; }
+void f91() /* [ */ { st.i[++r] = p; st.i[++r] = pop(); st.i[++r] = pop(); }
 void f92() /* \ */ { --s; }
 void f93() /* ] */ { ++st.i[r]; if (st.i[r]<=st.i[r-1]) { p=st.i[r-2]; } else { r-=3; } }
 void f94() /* ^ */ { p = st.i[r--]; }
-void f95() /* _ */ { TOS=-TOS; }
+void f95() /* _ */ { TOS = -TOS; }
 void f96() /* ` */ { char *y=ex; while (st.b[p]!='`') { *(y++)=st.b[p++]; } *y=0; system(ex); ++p; }
 void f97() /* a */ {}
 void f98() /* b */ { putc(' ', stdout); }
 void f99() /* c */ { u = st.b[p++];
     if (u == '@') { TOS=st.b[TOS]; }
     if (u == '!') { st.b[TOS]=NOS; s-=2; } }
-void f100() /* d */ { u=st.b[p++]; st.i[u+32]--; }
+void f100() /* d */ { u=st.b[p++]; st.i[u+ro]--; }
 /* e-h */ void f101() {} void f102() {} void f103() {} void f104() {} /* e-h */
-void f105() /* i */ { u=st.b[p++]; st.i[u+32]++; }
+void f105() /* i */ { u=st.b[p++]; st.i[u+ ro]++; }
 /* j-m */ void f106() {} void f107() {} void f108() {} void f109() {} /* j-m */
 void f110() /* n */ { putc('\n',stdout); }
 /* o-q */ void f111() {} void f112() {} void f113() {}
-void f114() /* r */ { u = st.b[p++]; push(st.i[u+32]); }
-void f115() /* s */ { u = st.b[p++]; st.i[u+32] = pop(); }
+void f114() /* r */ { u = st.b[p++]; push(st.i[u+ ro]); }
+void f115() /* s */ { u = st.b[p++]; st.i[u+ ro] = pop(); }
 /* t-w */ void f116() {} void f117() {} void f118() {} void f119() {} /* t-w */
 void f120() /* x */ { u = st.b[p++];
     if (u == 'U') { --r; }
@@ -73,14 +73,14 @@ f33,f34,f35,f36,f37,f38,f39,f40,f41,f42,f43,f44,f45,f46,f47,fN,fN,fN,fN,fN,fN,fN
 f58,f59,f60,f61,f62,f63,f64,fF,fF,fF,fF,fF,fF,fF,fF,fF,fF,fF,fF,fF,fF,fF,fF,fF,fF,fF,fF,fF,fF,fF,fF,fF,fF,
 f91,f92,f93,f94,f95,f96,f97,f98,f99,f100,f101,f102,f103,f104,f105,f106,f107,f108,f109,f110,f111,f112,f113,
 f114,f115,f116,f117,f118,f119,f120,f121,f122,f123,f124,f125,f126, };
-void run(int sb) { s=(s<1)?0:s; p=sb; r=35; while ((st.b[p]) && (p >= sb)) { u = st.b[p++]; q[u](); } }
-void loop() { printf(" s2\n"); fgets(&st.b[8000], 1000, stdin); run(8000); }
-int main(int argc, char* argv[]) {
-    for (i = 0; i < 2400; i++) { st.i[i] = 0; }
+void R(int sb) { s=(s<1)?0:s; p=sb; r=35; while (st.b[p]) { u = st.b[p++]; q[u](); } }
+void L() { v = m; printf("\ns2>"); fgets(&st.b[v], 99, stdin); R(v); }
+void main(int argc, char* argv[]) {
+    m = b; for (i = 0; i < 2500; i++) { st.i[i] = 0; }
     if (argc > 1) {
-        FILE* fp = fopen(argv[1], "rt"); if (!fp) { printf("file?"); exit(0); }
-        p = 8000; while ((c = fgetc(fp)) != EOF) { if (btw(c,32,126)) { st.b[p++]=c; } };
-        fclose(fp); run(8000);
+        FILE* fp = fopen(argv[1], "rt"); if (fp) {
+            while ((c = fgetc(fp)) != EOF) { if (btw(c,32,126)) { st.b[m++]=c; } };
+        fclose(fp); R(b); } else { printf("file?"); }
     }
-    while (1) { loop(); } return 0;
+    while (1) { L(); };
 }
