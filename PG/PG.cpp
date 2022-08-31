725e5ab6dccf1f41582d1a84e2484f22308d9772
Chris Curl <curlc@yahoo.com>
2022-08-31 01:43:04 -0400

    update

--- PG/PG.cpp
+++ PG/PG.cpp
@@ -1,18 +1,21 @@
 // pg.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 #include <stdio.h>
-#include <Windows.h>
+// #include <time.h>
 
-#define PGM_SZ 256
-#define VAR_SZ 2560
+#define PGM_SZ   65535
+#define DICT_SZ    511
+#define VAR_SZ   65535
+#define NAME_LEN    13
 
 typedef unsigned long CELL;
+typedef unsigned char byte;
 typedef void (*funcPtr)();
 typedef struct {
     funcPtr xt;
     byte flags;
-    byte sz;
-    char name[14];
+    byte len;
+    char name[NAME_LEN+1];
 } DICT_T;
 
 #define TOS stk[sp]
@@ -23,35 +26,38 @@ typedef struct {
 #define DROP2 sp-=2
 #define RPUSH(x) rstk[++rsp]=x
 #define RPOP rstk[rsp--]
+#define LPUSH(x) lstk[++lsp]=x
+#define LPOP lstk[lsp--]
 
-int ip, here, sp, rsp, vhere, base, state;
-funcPtr next;
-CELL t, n, stk[32], rstk[32];
-funcPtr pgm[PGM_SZ+1];
+int here, sp, rsp, lsp, vhere, base, state, last;
+funcPtr *ip, next;
+CELL t, n, stk[32], lstk[32];
+funcPtr pgm[PGM_SZ+1], *rstk[32];
 byte vars[VAR_SZ+1];
+DICT_T dict[DICT_SZ+1];
 
 void doNOP() { }
 void doBREAK() { /* TODO! */ }
-void doRETURN() { if (rsp) { ip=RPOP; } else { ip=PGM_SZ; pgm[ip]=0; } }
-void doCALL() { if (pgm[ip]!=doRETURN) { RPUSH(ip+1); } ip=(int)pgm[ip]; }
+void doEXIT() { if (rsp) { ip=RPOP; } else { ip=&pgm[PGM_SZ]; *ip=0; } }
+void doCOL() { while ((*ip) && (*ip != doEXIT)) { (*ip++)(); } ip=RPOP; }
 void doDUP() { t = TOS; PUSH(t); }
 void doSWAP() { t = TOS; TOS = NOS; NOS = t; }
 void doOVER() { t = NOS; PUSH(t); }
 void doDROP() { if (sp) { DROP; } }
-void doJMP() { ip = (CELL)pgm[ip]; }
-void doDO() { RPUSH(ip); RPUSH(NOS); RPUSH(POP); DROP; }
-void doI() { PUSH(rstk[rsp]); }
-void doJ() { PUSH(rstk[rsp-4]); }
-void doLOOP() { if (++rstk[rsp] < rstk[rsp-1]) { ip=rstk[rsp-2]; } else { rsp-=3; } }
+void doJMP() { ip = (funcPtr*)*ip; }
+void doDO() { RPUSH(ip); LPUSH(NOS); LPUSH(TOS); DROP2; }
+void doI() { PUSH(lstk[lsp]); }
+void doJ() { PUSH(lstk[lsp-4]); }
+void doLOOP() { if (++lstk[lsp] < lstk[lsp-1]) { ip=rstk[rsp]; } else { rsp--; lsp-=2; } }
 void doBEGIN() { RPUSH(ip); }
 void doWHILE() { if (POP) { ip=rstk[rsp]; } else { rsp--; } }
 void doAGAIN() { ip=rstk[rsp]; }
-void doLIT() { PUSH((CELL)pgm[ip++]); }
-void do0BRANCH() { t = (CELL)pgm[ip++]; if (POP == 0) { ip = t; } }
-void doDOTD() { printf("%d", POP); }
-void doDOTH() { printf("%x", POP); }
-void doEMIT() { printf("%c", POP); }
-void doSEMI() { pgm[here++] = doRETURN; state = 0; }
+void doLIT() { PUSH((CELL)*(ip++)); }
+void do0BRANCH() { if (POP == 0) { ip = (funcPtr*)*ip; } else { ++ip; } }
+void doDOTD() { printf("%ld", POP); }
+void doDOTH() { printf("%lx", POP); }
+void doEMIT() { printf("%c", (char)POP); }
+void doSEMI() { pgm[here++]=doEXIT; state = 0; }
 void doFETCH() { TOS=*(CELL*)&vars[TOS]; }
 void doCFETCH() { TOS = vars[TOS]; }
 void doSTORE() { *(CELL*)&vars[TOS] = NOS; DROP2; }
@@ -63,13 +69,44 @@ void doADD() { NOS += TOS; DROP; }
 void doSUB() { NOS -= TOS; DROP; }
 void doMUL() { NOS *= TOS; DROP; }
 void doDIV() { NOS /= TOS; DROP; }
+void doMOD() { NOS %= TOS; DROP; }
 void doAND() { NOS &= TOS; DROP; }
 void doOR()  { NOS |= TOS; DROP; }
 void doXOR() { NOS ^= TOS; DROP; }
 void doCOM() { TOS = ~TOS; }
+void doLAST() { PUSH((CELL)&last); }
+void doHERE() { PUSH((CELL)&here); }
+void doWORDS() { for (int l=last; 0<=l; l--) { printf("%s\t", dict[l].name); } }
+
+int strLen(const char *src) {
+    int len = 0;
+    while (*(src++)) { ++len; }
+    return len;
+}
+
+byte strCpy(char *dst, const char *src, byte len) {
+    char *x = dst;
+    int l = 0;
+    if (len < 1) { len=255; }
+    while (*src && (l < len)) {
+        *(x++) = *(src++);
+        ++l;
+    }
+    *x = 0;
+    return l;
+}
 
 void doCREATE() {
-    // TODO!!
+    ++last;
+    dict[last].len = strCpy(dict[last].name, (char *)POP, NAME_LEN);
+    dict[last].xt = (funcPtr)POP;
+    dict[last].flags = 0;
+}
+
+void create(const char *nm, funcPtr xt) {
+    PUSH((CELL)xt);
+    PUSH((CELL)nm);
+    doCREATE();
 }
 
 void doFIND() {
@@ -78,18 +115,27 @@ void doFIND() {
 }
 
 void init() {
-    here = sp = rsp = 0;
+    here = sp = rsp = lsp = 0;
+    last = -1;
+    for (int i = 0; i <= PGM_SZ; i++) { pgm[i] = 0; }
+
+    create("DUP", doDUP);
+    create("SWAP", doSWAP);
+    create("OVER", doOVER);
+
     pgm[here++] = doLIT;
     pgm[here++] = (funcPtr)12345;
     pgm[here++] = doLIT;
     pgm[here++] = (funcPtr)65;
     pgm[here++] = doSWAP;
+    pgm[here++] = doDOTD;
+    pgm[here++] = doEMIT;
     pgm[here++] = doLIT;
     pgm[here++] = (funcPtr)444555;
     pgm[here++] = do0BRANCH;
-    pgm[here++] = (funcPtr)333;
+    pgm[here++] = 0;
     pgm[here++] = doLIT;
-    pgm[here++] = (funcPtr)1000;
+    pgm[here++] = (funcPtr)100;
     pgm[here++] = doLIT;
     pgm[here++] = (funcPtr)0;
     pgm[here++] = doDO;
@@ -102,9 +148,9 @@ void init() {
     pgm[here++] = doLIT;
     pgm[here++] = (funcPtr)10;
     pgm[here++] = doDUP;
+    pgm[here++] = doDUP;
     pgm[here++] = doEMIT;
-    pgm[here++] = doEMIT;
-    pgm[here++] = doDOTD;
+    pgm[here++] = doWORDS;
     pgm[here++] = doEMIT;
     pgm[here++] = 0;
 }
@@ -112,7 +158,7 @@ void init() {
 int main()
 {
     init();
-    ip = 0;
-    next = pgm[ip++];
-    while (next) { next(); next = pgm[ip++]; }
+    ip = &pgm[0];
+    funcPtr n;
+    while (*ip) { (*ip++)(); }
 }
