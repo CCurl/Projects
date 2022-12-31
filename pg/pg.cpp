01682543c81a2e3b972eb6cac410a38a0ec7f80d
Chris Curl <curlbud-github.com>
2022-12-31 18:19:52 -0500

    More, but slower (same as s3 now)

--- pg/pg.cpp
+++ pg/pg.cpp
@@ -5,9 +5,8 @@
 #define NEXT goto next
 #define PS(x)  stk[++sp]=(x)
 #define PP     stk[sp--]
-#define S0     stk[sp]
-#define S1     stk[sp-1]
-#define S2     stk[sp-2]
+#define TOS    stk[sp]
+#define NOS    stk[sp-1]
 #define RPS(x) rstk[++rsp]=(x)
 #define RPP    rstk[rsp--]
 #define R0     rstk[rsp]
@@ -18,10 +17,12 @@
 #define L0     lstk[lsp]
 #define L1     lstk[lsp-1]
 #define L2     lstk[lsp-2]
+#define L3     lstk[lsp-3]
 #define BTW(a,b,c) ((b<=a)&&(a<=c))
 
 char u, *pc;
-long stk[32], rstk[32], lstk[30], sp, rsp, lsp, t;
+long stk[32], rstk[32], lstk[30], regs[26], sp, rsp, lsp, t;
+long locs[100], lb;
 
 void run(const char *x) {
     pc = (char *)x;
@@ -33,29 +34,29 @@ void run(const char *x) {
     case ' ': NEXT;
     case '!': printf("%c",u); NEXT;
     case '"': printf("%c",u); NEXT;
-    case '#': t=S0; PS(t); NEXT;
-    case '$': t=S0; S0=S1; S1=t; NEXT;
-    case '%': t=S1; PS(t); NEXT;
+    case '#': t=TOS; PS(t); NEXT;
+    case '$': t=TOS; TOS=NOS; NOS=t; NEXT;
+    case '%': t=NOS; PS(t); NEXT;
     case '&': printf("%c",u); NEXT;
     case '\'': D1; NEXT;
     case '(': if (PP==0) { while (*(pc++)!=')') {} } NEXT;
-    case ')': printf("%c",u); NEXT;
-    case '*': S1*=S0; D1; NEXT;
-    case '+': S1+=S0; D1; NEXT;
+    case ')': NEXT;
+    case '*': NOS*=TOS; D1; NEXT;
+    case '+': NOS+=TOS; D1; NEXT;
     case ',': printf("%c",u); NEXT;
-    case '-': S1-=S0; D1; NEXT;
+    case '-': NOS-=TOS; D1; NEXT;
     case '.': printf("%ld",PP); NEXT;
-    case '/': S1/=S0; D1; NEXT;
+    case '/': NOS/=TOS; D1; NEXT;
     case '0': case '1': case '2': case '3': 
     case '4': case '5': case '6': case '7': 
     case '8': case '9': PS(u-'0');
-        while (BTW(*pc,'0','9')) { S0=(S0*10)+(*(pc++)-'0'); }
+        while (BTW(*pc,'0','9')) { TOS=(TOS*10)+(*(pc++)-'0'); }
         NEXT;
     case ':': printf("%c",u); NEXT;
     case ';': printf("%c",u); NEXT;
-    case '<': S1=(S1<S0)?-1:0;  D1; NEXT;
-    case '=': S1=(S1==S0)?-1:0; D1; NEXT;
-    case '>': S1=(S1>S0)?-1:0;  D1; NEXT;
+    case '<': NOS=(NOS<TOS)?-1:0;  D1; NEXT;
+    case '=': NOS=(NOS==TOS)?-1:0; D1; NEXT;
+    case '>': NOS=(NOS>TOS)?-1:0;  D1; NEXT;
     case '?': printf("%c",u); NEXT;
     case '@': printf("%c",u); NEXT;
     case 'A': printf("%c",u); NEXT;
@@ -67,7 +68,7 @@ void run(const char *x) {
     case 'G': printf("%c",u); NEXT;
     case 'H': printf("%c",u); NEXT;
     case 'I': PS(L0); NEXT;
-    case 'J': printf("%c",u); NEXT;
+    case 'J': PS(L3); NEXT;
     case 'K': printf("%c",u); NEXT;
     case 'L': printf("%c",u); NEXT;
     case 'M': printf("%c",u); NEXT;
@@ -89,27 +90,44 @@ void run(const char *x) {
     case ']': if (++L0<L1) { pc=(char *)L2; }
         else { lsp-=3; } NEXT;
     case '^': printf("%c",u); NEXT;
-    case '_': printf("%c",u); NEXT;
+    case '_': TOS=-TOS; NEXT;
     case '`': printf("%c",u); NEXT;
     case 'a': printf("%c",u); NEXT;
     case 'b': printf(" "); NEXT;
     case 'c': printf("%c",u); NEXT;
-    case 'd': --S0; NEXT;
+    case 'd': u = *(pc++); if (BTW(u,'A','Z')) { --regs[u-'A']; }
+        else if (BTW(u,'0','9')) { --locs[lb+u-'0']; }
+        else { --pc; --TOS; }
+        NEXT;
     case 'e': printf("%c",u); NEXT;
     case 'f': printf("%c",u); NEXT;
     case 'g': printf("%c",u); NEXT;
-    case 'h': printf("%c",u); NEXT;
-    case 'i': ++S0; NEXT;
+    case 'h': PS(0); while (1) {
+            t=BTW(*pc,'0','9') ? *pc-'0' : -1;
+            if ((t<0) && BTW(*pc,'A','F')) { t=*pc-'A'+10; }
+            if (t<0) { break; }
+            TOS=(TOS*16)+t; ++pc;
+        } NEXT;
+    case 'i': u = *(pc++); if (BTW(u,'A','Z')) { ++regs[u-'A']; }
+        else if (BTW(u,'0','9')) { ++locs[lb+u-'0']; }
+        else { --pc; ++TOS; }
+        NEXT;
     case 'j': printf("%c",u); NEXT; 
     case 'k': printf("%c",u); NEXT;
-    case 'l': printf("%c",u); NEXT;
+    case 'l': u = *(pc++); if (u=='+') { lb+=(lb<90)?10:0; }
+        else if (u=='-') { lb=(0<lb)?10:0; }
+        NEXT;
     case 'm': printf("%c",u); NEXT;
     case 'n': printf("%c",u); NEXT;
     case 'o': printf("%c",u); NEXT;
     case 'p': printf("%c",u); NEXT;
     case 'q': printf("%c",u); NEXT;
-    case 'r': printf("%c",u); NEXT;
-    case 's': printf("%c",u); NEXT;
+    case 'r': u=*(pc++); if (BTW(u,'A','Z')) { PS(regs[u-'A']); }
+        else if (BTW(u,'0','9')) { PS(locs[lb+u-'0']); }; 
+        NEXT;
+    case 's': u=*(pc++);if (BTW(u,'A','Z')) { regs[u-'A']=PP; }
+        else if (BTW(u,'0','9')) { locs[lb+u-'0']=PP; }; 
+        NEXT;
     case 't': printf("%c",u); NEXT;
     case 'u': printf("%c",u); NEXT;
     case 'v': printf("%c",u); NEXT;
@@ -125,7 +143,7 @@ void run(const char *x) {
 }
 
 int main() {
-    sp = rsp = lsp = 0;
+    sp = rsp = lsp = lb = 0;
     run("20 0[T500 1000#**0[]T$-.b]N");
     run("20 0[T200 1000#**{d#}\\T$-.b]");
 }
