eaeacf664d0ce5ef6398bd3cda74c444a10aad99
Chris Curl <curlbud-github.com>
2023-03-17 09:06:02 -0400

    more extract

--- c3/sandbox.f
+++ c3/sandbox.f
@@ -5,35 +5,29 @@ load strings.f
 
 : extract-field ( from to fld delim-- )
     +regs s4 s3 s2 count drop s1
+    r2 str-trunc drop
+    r3 1 < if -regs exit then
     r3 1 > if 
         r3 1 do r1 r4 str-findc s1 
             r1 if i1 else r3 +i then
         loop
     then
-    r2 str-trunc drop
     r1 0= if -regs exit then
     0 s8 begin 
-        r1 c@ s5 r5 0= r5 r4 = or s8
-        r8 0= if r5 r2 str-catc drop i1 then
+        r1 c@ s5 r5 0= r5 r4 = or
+        if 1 s8 else r5 r2 str-catc drop i1 then
     r8 until -regs ;
 
-vhere 100 + s1 s" 111,222,333,444" r1 1 ',' extract-field r1 count type
-vhere 100 + s1 s" 111,222,333,444" r1 2 ',' extract-field ." ," r1 count type
-vhere 100 + s1 s" 111,222,333,444" r1 3 ',' extract-field ." ," r1 count type
-vhere 100 + s1 s" 111,222,333,444" r1 4 ',' extract-field ." ," r1 count type
-vhere 100 + s1 s" 111,222,333,444" r1 5 ',' extract-field ." ," r1 count type
+vhere 100 + s1
+s" aaa" r1 1 ',' extract-field r1 count type ." ,"
+s" 111,222,333,444" r1 1 ',' extract-field r1 count type ." ,"
+s" 111,222,333,444" r1 2 ',' extract-field r1 count type ." ,"
+s" 111,222,333,444" r1 3 ',' extract-field r1 count type ." ,"
+s" 111,222,333,444" r1 4 ',' extract-field r1 count type ." ,"
+s" 111,222,333,444" r1 5 ',' extract-field r1 count type ." ,"
+s" 111,222,333,444" r1 9 ',' extract-field r1 count type ." ,"
 
 load-abort
-
-
-    0( skip to nth fld ... ) 
-    r3 1 [r1 r4 sf 1+ s1]
-    0( collect num into r2/to-str .. )
-    r2 s5 0( r5: temp char ptr )
-    {r1 c@ r5 c! i1 i5 r1 c@ r4 = ~} 0 r5 c! 
-    r2 xE sC rC " -extracted integer %d- " 
-    rC rM >( " new peak " rC sM )
-
 
 \ Some trigonometry stuff ...
 var xf  (var) (xf)  : >xf  (xf)  ! ;
