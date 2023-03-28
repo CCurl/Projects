18a339a05e4df9dce56356495496953eac61e2df
Chris Curl <curlc@yahoo.com>
2023-03-28 00:59:25 -0400

    editor updates

--- c3/editor.f
+++ c3/editor.f
@@ -42,7 +42,8 @@ val bottom  (val) (bottom)
 
 : clear-buf ( -- )    buf buf-sz 0 fill ;
 : line-addr ( n--a )  max-width * buf + ;
-: row->addr ( --a )   row top +   line-addr ;
+: scr-row   ( --a )   row top + ;
+: row->addr ( --a )   scr-row   line-addr ;
 : rc->pos   ( -- )    row->addr   col +   >pos ;
 : ->cur     ( -- )    col 1+   row 1+   ->yx ;
 
@@ -56,6 +57,35 @@ val bottom  (val) (bottom)
     begin r1 c@ if 0 else d1 1 then while
     begin r1 c@ if i1 1 else 0 then while
     r1 -regs ;
+
+: insert-line ( num-- )
+    +regs   line-addr s1   max-width s4
+    r1 r4 + s2   find-eof r4 + s9
+    begin
+        r9 r4 - c@ r9 c!
+        d9   r9 r2 >=
+    while
+    r4 0 do 0 r1 i + c! loop
+    -regs ;
+
+: delete-line ( num-- )
+    +regs max-width s4 line-addr s1
+    find-eof s2   r2 r4 - s9
+    clip-board r1 s-from-sz
+    begin
+        r1 r4 + c@ r1 c!
+        i1   r1 r9 <=
+    while
+    r4 0 do 0 r9 i + c! loop
+    -regs ;
+
+: put-line ( -- )
+    scr-row insert-line
+    +regs  scr-row line-addr s1
+    clip-board count ?dup if
+        0 do dup c@ r1 i + c! 1+ loop
+    then drop
+    -regs refresh ;
 
 : load-file ( fh-- )
     >r
@@ -84,7 +114,7 @@ val bottom  (val) (bottom)
 
 : show-footer ( -- )
     cr cr ." cb: [" clip-board count type ." ], "
-    fn count type msg count type ;
+    fn count type msg count type clr-eol ;
 
 : do-refresh ( -- )
     refresh? if
@@ -106,6 +136,9 @@ val bottom  (val) (bottom)
 : home?      'A' = ;
 : end?       'D' = ;
 : del-ch?    'x' = ;
+: del-ln?    'X' = ;
+: ins-ln?    'O' = ;
+: put-ln?    'P' = ;
 : repl-md?   'R' = ;
 : ins-md?    'i' = ;
 : cmd-md?    ':' = ;
@@ -113,6 +146,7 @@ val bottom  (val) (bottom)
 : scr-down?  'S' = ;
 : pg-up?     'E' = ;
 : pg-dn?     'C' = ;
+: yank?      'Y' = ;
 
 : left   col 1- 0         max >col   ->cur ;
 : right  col 1+ max-width min >col   ->cur ;
@@ -149,6 +183,9 @@ val bottom  (val) (bottom)
 : replace-ch ( ch-- )  pos c!   col++   refresh ;
 : insert-ch  ( ch-- )  1 insert-num   replace-ch ;
 : delete-ch  ( -- )    1 delete-num   refresh ;
+: ins-line ( -- ) scr-row insert-line refresh ;
+: del-line ( -- ) scr-row delete-line refresh ;
+: yank ( -- ) clip-board scr-row line-addr s-from-sz refresh ;
 : command-ch ( -- )    r1 emit
     'W' r1 = if fn write-file exit then
     'Q' r1 = if 1 done ! exit then ;
@@ -172,6 +209,9 @@ val bottom  (val) (bottom)
     r1 home?      if home          exit then
     r1 end?       if end           exit then
     r1 del-ch?    if delete-ch     exit then
+    r1 ins-ln?    if ins-line      exit then
+    r1 put-ln?    if put-line      exit then
+    r1 del-ln?    if del-line      exit then
     r1 repl-md?   if repl-md       exit then
     r1 ins-md?    if ins-md        exit then
     r1 cmd-md?    if cmd-md        exit then
@@ -179,6 +219,7 @@ val bottom  (val) (bottom)
     r1 pg-dn?     if pg-dn         exit then
     r1 scr-up?    if scroll-up     exit then
     r1 scr-down?  if scroll-down   exit then
+    r1 yank?      if yank          exit then
     r1 . ;
 
 : edit-loop ( -- )
@@ -202,4 +243,4 @@ val bottom  (val) (bottom)
     fn read-file
     cls edit-loop ;
 
-edit xxx.f
+edit editor.f
