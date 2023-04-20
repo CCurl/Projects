c11ca389e1738d5ea1a802d7e00385babb0c1a04
Christopher Curl <ccurl@mmm.com>
2023-04-20 13:52:50 -0400

    update work.f

--- c3/work.f
+++ c3/work.f
@@ -8,6 +8,8 @@ load string.f
 variable p 100 allot
 
 : words-n last swap 1+ 1 do .word tab i 10 mod 0= if cr then word-sz + loop drop ;
+: edit s" nvim ." system ;      
+: pull s" git pull" system ;      
 : pw s" pw" system ;
 : li s" chrome https://fwut-proc-a.mmm.com:1443/netaccess/loginuser.html" system ;
 : yahoo s" chrome http://mail.yahoo.com" system ;
@@ -16,4 +18,4 @@ variable p 100 allot
 : gz-bw  ( n-- ) p s" gz-bw "     s-cpy p s-scatn p system ;
 : gz-app ( n-- ) p s" gz-server " s-cpy p s-scatn p s"  a" p s-cat p system ;
 : gz-web ( n-- ) p s" gz-server " s-cpy p s-scatn p s"  w" p s-cat p system ;
-." words added: " 10 words-n
+." words added: " 12 words-n
