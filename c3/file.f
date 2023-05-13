5357122d4866f9c94c70a1cd8f4a4f8a1ca5d477
Chris Curl <curlbud-github.com>
2023-05-13 11:30:15 -0400

    some file handline updates

--- c3/file.f
+++ c3/file.f
@@ -7,11 +7,15 @@
 : fopen-wt ( fn--fh ) s" wt" fopen ;
 : fopen-wb ( fn--fh ) s" wb" fopen ;
 : fopen-rw ( fn--fh ) s" r+b" fopen ;
+
+variable T0
+: sv-output (output_fp) @ T0 ! ;
+: rs-output T0 @ (output_fp) ! ;
+: ch-output sv-output (output_fp) ! ;
 : ->stdout 0 (output_fp) ! ;
 
 variable T0
-: fgetc ( fh--c f )
-    >r T0 1 r> fread T0 c@ swap ;
+: fgetc ( fh--c f )  T0 swap 1 swap fread T0 c@ swap ;
 
 : fgets ( a fh--len eof )
     +regs   s2 s1   0 s4
@@ -19,13 +23,9 @@ variable T0
         r2 fgetc   swap s3
         0= if 0 r1 c!   r4 1 -regs exit then
         r3 10 = if  0 r1 c!  r4 0 -regs exit then
-        r3 13 = if  0 s3 then
-        r3  9 = if 32 s3 then
-        r3 if r3 r1 c!   i1 i4 then
+        r3 13 <> if r3 r1+ c!   i4 then
     again ;
 
-: fputc ( c fh-- )
-    (output_fp) @ >r (output_fp) ! emit r> (output_fp) ! ;
-
-: fputs ( str fh-- )
-    (output_fp) @ >r (output_fp) ! count type r> (output_fp) ! ;
+: fputc   ( c fh-- )     ch-output emit rs-output ;
+: fputs   ( str fh-- )   ch-output count type rs-output ;
+: fputs-z ( strz fh-- )  ch-output count typez rs-output ;
