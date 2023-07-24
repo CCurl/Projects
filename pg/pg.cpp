a6e753266614f3eb8d5c80b59c56a7ecf9966851
Christopher Curl <ccurl@mmm.com>
2023-07-24 10:39:19 -0400

    updates (mainly dis.c3)

--- pg/pg.cpp
+++ pg/pg.cpp
@@ -1,270 +1,175 @@
-#include <stdlib.h>
 #include <stdio.h>
-#include <time.h>
 
-#define CODESZ      1000
-#define DICTSZ      1000
+#define BTW(x,l,h) ((l<=x)&&(x<=h))
 
-#define NCASE       goto next; case
-#define NEXT        goto next
-#define PS(x)       stk[++sp]=(x)
-#define POP         stk[sp--]
-#define S0          stk[sp]
-#define S1          stk[sp-1]
-#define S2          stk[sp-2]
-#define RPS(x)      rstk[++rsp]=(x)
-#define RPOP        rstk[rsp--]
-#define R0          rstk[rsp]
-#define R1          rstk[rsp-1]
-#define R2          rstk[rsp-2]
-#define D1          sp--
-#define D2          sp-=2
-#define L0          lstk[lsp]
-#define L1          lstk[lsp-1]
-#define L2          lstk[lsp-2]
-#define BTW(a,b,c)  ((b<=a)&&(a<=c))
-#define HERE        vars.l[0]
-#define LAST        vars.l[1]
+typedef struct token_t {
+	int token;
+	char id[32];
+	char *str;
+} TOK_T;
 
-#define WORDSZ      sizeof(long)
-#define OPIR(x)     x.ir[3]
-#define OPARG(x)    (x.wd&0x00ffffff)
-#define u           OPIR(code[pc-1])
+TOK_T tokStk[32];
+FILE *fp, *fpStk[32];
+char tib[132], *in;
+int fSP, tokSP, err;
 
-#define IMMED       0x01
-#define INLINE      0x02
-#define LIT          1
+char lCase(char c) { return BTW(c,'A','Z') ? c-32: c; }
 
-typedef unsigned char byte;
-typedef unsigned long ulong;
-
-typedef union {
-    ulong wd;
-    byte ir[WORDSZ];
-} op_t;
-
-typedef struct {
-    ulong addr;
-    byte attr[2];
-    byte flgs;
-    byte len;
-    char name[16];
-} dict_t;
-
-unsigned long ir;
-long stk[32], rstk[32], lstk[30], sp, rsp, lsp, t;
-union { byte b[10000*sizeof(long)]; long l[10000]; } vars;
-char *toIN, wd[32];
-op_t code[CODESZ];
-dict_t dict[DICTSZ];
-
-void run(long pc) {
-    next:
-
-    switch(OPIR(code[pc++])) {
-        NCASE LIT: PS(OPARG(code[pc-1]));
-        NCASE ' ':
-        NCASE '!': vars.l[S0] = S1; D2;
-        NCASE '"': while (0) {}
-        NCASE '#': t=S0; PS(t);
-        NCASE '$': t=S0; S0=S1; S1=t;
-        NCASE '%': t=S1; PS(t);
-        NCASE '&': printf("%c",u);
-        NCASE '\'': D1;
-        NCASE '(': printf("%c",u);
-        NCASE ')': printf("%c",u);
-        NCASE '*': S1*=S0; D1;
-        NCASE '+': S1+=S0; D1;
-        NCASE ',': printf("%c",u);
-        NCASE '-': S1-=S0; D1;
-        NCASE '.': printf(" %ld",POP);
-        NCASE '/': S1 /= ((S0)?S0:1); D1;
-        NCASE '0': case '1': case '2': case '3': 
-        NCASE '4': case '5': case '6': case '7': 
-        NCASE '8': case '9': printf("%c",u);
-        NCASE ':': RPS(pc); pc = OPARG(code[pc-1]);
-        NCASE ';': pc = (0<rsp) ? RPOP : 0;
-        NCASE '<': S1=(S1<S0)?-1:0;  D1;
-        NCASE '=': S1=(S1==S0)?-1:0; D1;
-        NCASE '>': S1=(S1>S0)?-1:0;  D1;
-        NCASE '?': printf("%c",u);
-        NCASE '@': S0 = vars.l[S0];
-        NCASE 'a': PS(a);
-        NCASE 'A': printf("%c",u);
-        NCASE 'B': printf("%c",u);
-        NCASE 'C': S0 = vars.b[S0];
-        NCASE 'D': printf("%c",u);
-        NCASE 'E': printf("%c",u);
-        NCASE 'F': printf("%c",u);
-        NCASE 'G': printf("%c",u);
-        NCASE 'H': printf("%c",u);
-        NCASE 'I': PS(L0);
-        NCASE 'J': printf("%c",u);
-        NCASE 'K': printf("%c",u);
-        NCASE 'L': printf("%c",u);
-        NCASE 'M': printf("%c",u);
-        NCASE 'N': printf("\n");
-        NCASE 'O': printf("%c",u);
-        NCASE 'P': printf("%c",u);
-        NCASE 'Q': exit(0);
-        NCASE 'R': printf("%c",u);
-        NCASE 'S': printf("%c",u);
-        NCASE 'T': PS(clock());
-        NCASE 'U': printf("%c",u);
-        NCASE 'V': printf("%c",u);
-        NCASE 'W': printf("%c",u);
-        NCASE 'X': printf("%c",u);
-        NCASE 'Y': printf("%c",u);
-        NCASE 'Z': printf("%c",u);
-        NCASE '[': lsp+=3; L0=POP; L1=POP; L2=pc;
-        NCASE '\\': if (0<sp) sp--;
-        NCASE ']': if (++L0<L1) { pc=L2; } else { lsp-=3; }
-        NCASE '^': printf("%c",u);
-        NCASE '_': printf("%c",u);
-        NCASE '`': printf("%c",u);
-        NCASE 'a': printf("%c",u);
-        NCASE 'b': printf("%c",u);
-        NCASE 'c': S0 = vars.b[S0];
-        NCASE 'd': --S0;
-        NCASE 'e': printf("%c",(char)POP);
-        NCASE 'f': printf("%c",u);
-        NCASE 'g': printf("%c",u);
-        NCASE 'h': printf("%c",u);
-        NCASE 'i': ++S0;
-        NCASE 'j': printf("%c",u); 
-        NCASE 'k': printf("%c",u);
-        NCASE 'l': printf("%c",u);
-        NCASE 'm': printf("%c",u);
-        NCASE 'n': printf("%c",u);
-        NCASE 'o': printf("%c",u);
-        NCASE 'p': printf("%c",u);
-        NCASE 'q': printf("%c",u);
-        NCASE 'r': printf("%c",u);
-        NCASE 's': printf("%c",u);
-        NCASE 't': printf("%c",u);
-        NCASE 'u': printf("%c",u);
-        NCASE 'v': printf("%c",u);
-        NCASE 'w': printf("%c",u);
-        NCASE 'x': printf("%c",u);
-        NCASE 'y': printf("%c",u);
-        NCASE 'z': printf("%c",u);
-        NCASE '{': lsp+=3; L2=(long)pc;
-        NCASE '|': printf("%c",u);
-        NCASE '}': if (POP) { pc=L2; } else { lsp -=3; }
-        NCASE '~': printf("%c",u); NEXT;
-        NCASE 0:
-        default: return;
-    }
+int strLen(const char *x) {
+	int l=0;
+	while (*(x++)) ++l;
+	return l;
 }
 
-long compile(byte op, ulong arg) {
-    code[HERE].wd = arg;
-    OPIR(code[HERE++]) = op;
-    return HERE-1;
+int strEq(const char *x, const char *y) {
+	while (*x && *y && (*x == *y)) { x++; y++; }
+	return (*x|*y) ? 0 : 1;
 }
 
-int doWord() {
-    int l=0;
-    while (BTW(*toIN,1,32)) { ++toIN; }
-    while (32 < *toIN) { wd[l++] = *(toIN++); }
-    wd[l] = 0;
-    return l;
+int strEqI(const char *x, const char *y) {
+	while (*x && *y && (lCase(*x) == lCase(*y))) { x++; y++; }
+	return (*x|*y) ? 0 : 1;
 }
 
-int doNum() {
-    if (BTW(*toIN,'0','9')) {
-        long num = 0;
-        while BTW(*toIN,'0','9') { num = (num*10) + *(toIN++) - '0'; }
-        compile(LIT, num);
-        return 1;
-    }
-    return 0;
+char *rTrim(char *x) {
+	int l=strLen(x);
+	while (l && (x[l-1]<33)) { --l; x[l] = 0; }
+	return x;
 }
 
-int doCreate(byte flgs) {
-    int l = doWord();
-    if (l==0) { return 0; }
-    dict_t *dp = &dict[LAST++];
-    dp->addr = HERE;
-    dp->flgs = flgs;
-    dp->attr[0] = dp->attr[1] = 0;
-    if (15<l) { l = 15; }
-    dp->len = l;
-    for (int i=0; i<=l; i++) { dp->name[i] = wd[i]; }
-    return 1;
+char *lTrim(char *x) {
+	while (*x && (*x < 33)) { ++x; }
+	return x;
 }
 
-int strEq(const char *s1, const char *s2) {
-    while (*s1 && *s2) {
-        if (*s1 == *s2) { ++s1; ++s2; }
-        else { return 0; }
-    }
-    return (*s1 == *s2) ? 1 : 0;
+char PK() { return *in; }
+
+char NC() {
+	while ( PK() && (PK() < 33) ) { ++in; }
+	if (PK() == 0) { return 0; }
+	return *(in++);
 }
 
-int doFind(char *nm) {
-    int l = 0;
-    while (nm[l]) { ++l; }
-    for (int i = LAST-1; 0 <= i; i--) {
-        dict_t *dp = &dict[i];
-        if ((dp->len == l) && strEq(nm, dp->name)) { return i; }
-    }
-    return -1;
+char *nextLine() {
+	tib[0] = 0;
+	while (fp && feof(fp)) {
+		fclose(fp);
+		fp = NULL;
+		if (0 < fSP) {
+			fp = fpStk[fSP--];
+			return nextLine();
+		}
+	}
+
+	if ((fp) && (fgets(tib, sizeof(tib), fp) == tib)) {
+		return rTrim(tib);
+	}
+	return 0;
 }
 
-int doDict() {
-    int l = doWord();
-    if (l==0) { return 0; }
-    if (strEq(wd, ":")) { return doCreate(0); }
-    if (strEq(wd,":i")) { return doCreate(INLINE); }
-    if (strEq(wd,":I")) { return doCreate(IMMED); }
-    int f = doFind(wd);
-    if (f < 0) {
-        for (int i = 0; i < l; i++) { compile(wd[i], 0); }
-        return 1;
-    }
-    if (dict[f].flgs & IMMED) {
-        run(dict[f].addr);
-    } else if (dict[f].flgs & INLINE) {;
-        long x = dict[f].addr;
-        compile(OPIR(code[x]), OPARG(code[x]));
-        while (OPIR(code[++x]) != ';') { compile(OPIR(code[x]), OPARG(code[x])); }
-    } else {
-        compile(':', dict[f].addr);
-    }
-    return 1;
+enum { EOS = 1, EOB, ID,  };
+char idBuf[32];
+
+int getID(char c) {
+	int l = 0;
+	if (c) { idBuf[l++] = c; }
+	else { in = lTrim(in); }
+	while (PK()) {
+		c = lCase(PK());
+		if (BTW(c,'a','z') || BTW(c,'0','9') || (c=='_')) {
+			idBuf[l++] = NC();
+		} else {
+			break;
+		}
+	}
+	idBuf[l] = 0;
+	printf("-%d:%s-", l, idBuf);
+	return l;
 }
 
-long doParse(const char *src) {
-    toIN = (char*)src;
-    long st = HERE;
-    while (*toIN) {
-        while (BTW(*toIN,1,32)) { ++toIN; continue; }
-        if (doNum()) { continue; }
-        if (doDict()) { continue; }
-    }
-    compile(0,0);
-    return st;
+int tokIdent() {
+	while (*in) {
+		++in;
+	}
+	return 0;
 }
 
-void defNum(const char *nm, long val) {
-    toIN = (char*) nm;
-    doCreate(INLINE);
-    compile(LIT, val);
-    compile(';', 0);
-    printf("-%s: %08lx-", nm, val);
+int doErr(const char *msg) {
+	printf("ERR: %s", msg);
+	++err;
+	return 0;
 }
 
-int main() {
-    sp = rsp = lsp = 0;
-    HERE = 0;
-    LAST = 0;
-    doParse(": here 0 @ ; : last 1 @ ;");
-    doParse(":I if here ; :I then last $! ;");
-    doParse(":i begin { ; :i while } ;");
-    doParse(":i do    [ ; :i loop  ] ;");
-    doParse(" : timer T ;  : elapsed timer $ - . ;");
-    run(doParse("timer 500 1000 # * * # . 0 do loop elapsed N"));
-    run(doParse("T 200 1000 #**#.{ d # }\\T$-.N"));
-    run(doParse("here . last . N"));
+int doPragma() {
+	if (getID(0) == 0) { return doErr("invalid #pragma"); }
+	if (strEq(idBuf, "include")) {
+		if (getID(0) == 0) { return doErr("invalid #include"); }
+		printf("-include [%s]-", idBuf);
+	} else {
+		doErr("invalid #pragma!");
+	}
+	return 0;
+}
+
+int stkKW(int KW) {
+	printf("-%s,kw:%d-", idBuf, KW);
+	return KW;
+}
+
+int checkKW() {
+	if (strEq(idBuf,"begin")) return stkKW(1);
+	if (strEq(idBuf,"while")) return stkKW(2);
+	if (strEq(idBuf,"repeat")) return stkKW(3);
+	if (strEq(idBuf,"again")) return stkKW(4);
+	if (strEq(idBuf,"if")) return stkKW(5);
+	if (strEq(idBuf,"else")) return stkKW(6);
+	if (strEq(idBuf,"then")) return stkKW(7);
+	return 0;
+}
+
+int doID(char c) {
+	int l = getID(c);
+	if (checkKW()) { return 1; }
+	return 1;
+}
+
+int nextToken() {
+	in = lTrim(in);
+	char c = NC();
+	if (c == '#') { return doPragma(); }
+	if (BTW(c,'A','Z')) { return doID(c); }
+	if (BTW(c,'a','z')) { return doID(c); }
+	if (c == '_') { return doID(c); }
+	return 0;
+}
+
+void doGenerate() {
+	for (int i=1; 0<=tokSP; i++) {
+		TOK_T *t = &tokStk[i];
+		printf("%d: %d, [%s], %p", i, t->token, t->id, t->str );
+		if (t->str) { printf(", [%s]", t->str); }
+		printf("\n");
+	}
+	tokSP = 0;
+}
+
+void parseLine() {
+	in = nextLine();
+	printf("[%s]\n", in);
+	while (in && PK()) { nextToken(); }
+}
+
+int main(int argc, char *argv[]) {
+	for (int i = 1; i < argc; i++) {
+		printf("%d: [%s]", i, argv[i]);
+	}
+	fSP = tokSP = 0;
+	fp = NULL;
+	if (argc>1) {
+		fp = fopen(argv[1],"rt");
+	}
+	while (fp && (err<10)) {
+		parseLine();
+	}
+	return 0;
 }
---
+++ stable/orig.c
@@ -0,0 +1,51 @@
+/* stable.c  interpreter: ver: 01.sep.2016 */
+#include <stdio.h>
+static int  i,v,m,r,p=0,n,mx,s,f,st[2000]; 
+static char t=0,c,u,a,k=0,pr[2000];
+N(){;} 
+f33(){st[st[v]]=st[s]; s--; }
+f34(){p++;u=pr[p];while(u!='\"'){putc(u,stdout);p++;u=pr[p];}}
+f35(){s++; st[s]=st[s-1];}
+f36(){i=st[s]; st[s]=st[s-1]; st[s-1]=i;}
+f37(){st[s-1]%=st[s]; s--; } 
+f38(){st[s-1]&=st[s]; s--; }
+f39(){p++;u=pr[p];while(u!='\''){s++;st[s]=u;p++;u=pr[p];}}
+f40(){if(st[s]==0){s--;p++;u=pr[p];while(u!=')'){p++;u=pr[p];}} else{s--;}}
+f42(){st[s-1]*=st[s]; s--; }
+f43(){ if(k==0){st[s-1]+=st[s]; s--; }else{st[v]++;} }
+f44(){printf("%c",st[s]); s--; }
+f45(){ if(k==0){st[s-1]-=st[s]; s--; }else{st[v]--;} }
+f46(){printf("%d",st[s]); s--;} f47(){st[s-1]/=st[s]; s--; }
+f9(){i=0;while((u>='0')&&(u<='9')){i=i*10+u-'0';p++;u=pr[p];}s++;st[s]=i;p--;}
+f58(){st[v]=st[s]; s--; }
+f59(){s++; st[s]=st[v]; }
+f60(){if(st[s]> st[s-1]){st[s]=-1;}else{st[s]=0;}}
+f61(){if(st[s]==st[s-1]){st[s]=-1;}else {st[s]=0;}}
+f62(){if(st[s]< st[s-1]){st[s]=-1;}else{st[s]=0;}}
+f63(){s++; st[s]=st[st[v]]; }
+f64(){i=st[s];st[s]=st[s-2];st[s-2]=st[s-1];st[s-1]=i;}
+fA(){r++; st[r]=p; p=st[u-35]; u=pr[p]; p--;}
+f91(){r++;st[r]=p;if(st[s]==0){p++;u=pr[p]; while(u!=']'){ p++; u=pr[p];}}}
+f92(){s--;} f93(){if(st[s]!=0) p=st[r]; else r--; s--; }
+f94(){if((c=getc(stdin))==EOF) { c=0; } s++; st[s]=c; }
+f95(){st[s]=-st[s]; }f96(){putc(u,stdout);} 
+fa(){k=1; v=u-97; }
+f123(){f=pr[p+1]-35;st[f]=p+2;while(u!='}'){p++;u=pr[p];}}
+f124(){st[s-1]|=st[s]; s--; }
+f125(){p=st[r];r--;}
+f126(){st[s]=  ~st[s]; }
+int (*q[127])()={
+N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,
+f33,f34,f35,f36,f37,f38,f39,f40,N,f42,f43,f44,f45,f46,f47,f9,f9,f9,
+f9,f9,f9,f9,f9,f9,f9,f58,f59,f60,f61,f62,f63,f64,fA,fA,fA,fA,fA,fA,
+fA,fA,fA,fA,fA,fA,fA,fA,fA,fA,fA,fA,fA,fA,fA,fA,fA,fA,fA,fA,f91,f92,
+f93,f94,f95,f96,fa,fa,fa,fa,fa,fa,fa,fa,fa,fa,fa,fa,fa,fa,fa,fa,fa,
+fa,fa,fa,fa,fa,fa,fa,fa,fa,f123,f124,f125,f126};
+int main(int argc,char *argv[]){ FILE *be; 
+ if (!(be=fopen(argv[1],"rb"))) {printf("pr?\n"); return 0;}
+ while((c=fgetc(be))!=EOF){ pr[p++]=c; } 
+ fclose(be); mx=p; i=1999; while(i--){ st[i]=0;}  
+ for(i=0;i<argc;i++) if(i>1) st[i-2]=atoi(argv[i]);
+ p=0; s=100; r=60; 
+ while(p<=mx){ u=pr[p]; q[u](); if((u<'a')||('z'<u))k=0; p++; }
+ return 0;}
