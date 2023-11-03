da50dbaee91a110a390c7bcccb1fb04acb79887c
Christopher Curl <ccurl@mmm.com>
2023-11-03 15:09:00 -0400

    some cleanup

--- pg2/pg2.c
+++ pg2/pg2.c
@@ -3,12 +3,12 @@
 #include <time.h>
 #include <stdint.h>
 
-#define NEXT   goto next
+#define NEXT   goto stkNext
 #define NCASE  NEXT; case
-#define NEXT2  goto next2
-#define NCASE2 NEXT2; case
+#define NEXT2  goto regNext
+#define RCASE NEXT2; case
 #define BCASE  break; case
-#define RCASE  return; case
+#define RETCASE  return; case
 #define PS(x)  stk[++sp]=(cell_t)(x)
 #define PP     stk[sp--]
 #define PP32   (int32_t)stk[sp--]
@@ -39,84 +39,87 @@ int sp, rsp, lsp, locBase, here, u, pc;
 
 #define OPC(x)   op->c[x]
 #define OP32(x)  op->i32[x]
+#define OP64     op->i64
 
-void dumpOp(op_t *op) {
-    printf("\npc: %d: ", pc-1);
-    printf("[0:%d%c]", OPC(0), BTW(OPC(0),32,126) ? (char)OPC(0) : '.');
-    for (int i=1; i<CELL_SZ; i++) { printf("[%d,%02d]", i, OPC(i)); }
+void dumpOp(int a, op_t *op) {
+    printf("\npc: %-3d - ", a);
+    printf("%2x/%c", OPC(0), BTW(OPC(0),32,126) ? (char)OPC(0) : '.');
+    for (int i=1; i<CELL_SZ; i++) { printf(",%2x", OPC(i)&0xff); }
+    printf(" - i32: %8lx,%8lx", OP32(0), OP32(1));
+    printf(" - i64: %jd / %jx", OP64, OP64);
 }
 
 void runReg(int start) {
     pc = start;
-    next2:
+    regNext:
     op = &code[pc++];
-    // dumpOp(op);
+    // dumpOp(pc, op);
     switch (OPC(0)) {
         case  0: return;
         case   '+': reg[OPC(3)] = reg[OPC(1)] + reg[OPC(2)];
-        NCASE2 '*': reg[OPC(3)] = reg[OPC(1)] * reg[OPC(2)];
-        NCASE2 '-': reg[OPC(3)] = reg[OPC(1)] - reg[OPC(2)];
-        NCASE2 '/': reg[OPC(3)] = reg[OPC(1)] / reg[OPC(2)];
-        NCASE2 '.': printf(" %jd", reg[OPC(1)]);
-        NCASE2 ',': printf("%c",  (char)reg[OPC(1)]);
-        NCASE2 '[': lsp+=3; L0=PP; L1=PP; L2=pc;
+        RCASE '*': reg[OPC(3)] = reg[OPC(1)] * reg[OPC(2)];
+        RCASE '-': reg[OPC(3)] = reg[OPC(1)] - reg[OPC(2)];
+        RCASE '/': reg[OPC(3)] = reg[OPC(1)] / reg[OPC(2)];
+        RCASE '.': printf(" %jd", reg[OPC(1)]);
+        RCASE ',': printf("%c",  (char)reg[OPC(1)]);
+        RCASE '[': lsp+=3; L0=PP; L1=PP; L2=pc;
             // printf("-[%ld:%ld]-", L0, L1);
-        NCASE2 ']': if (++L0<L1) { pc=(int)L2; } else { lsp-=3; }
-        NCASE2 't': t=clock(); if (BTW(OPC(1),0,25)) { reg[OPC(1)]=t; } else { PS(t); }
-        NCASE2 'l': PS(loc[locBase+OPC(1)]);
-        NCASE2 'L': loc[locBase+OPC(1)] = PP;
-        NCASE2 'm': reg[OPC(1)] = OP32(1);
-        NCASE2 'M': loc[locBase+OPC(1)] = OP32(1);
-        NCASE2 'n': PS(OP32(1));
+        RCASE ']': if (++L0<L1) { pc=(int)L2; } else { lsp-=3; }
+        RCASE 't': t=clock(); if (BTW(OPC(1),0,25)) { reg[OPC(1)]=t; } else { PS(t); }
+        RCASE 'l': PS(loc[locBase+OPC(1)]);
+        RCASE 'L': loc[locBase+OPC(1)] = PP;
+        RCASE 'm': reg[OPC(1)] = OP32(1);
+        RCASE 'M': loc[locBase+OPC(1)] = OP32(1);
+        RCASE 'n': PS(OP32(1));
             // printf("-n[%d]-", OP32(1));
-        NCASE2 'r': PS(reg[OPC(1)]);
+        RCASE 'r': PS(reg[OPC(1)]);
             // printf("-r[%d]-", OP32(1));
-        NEXT2; default: dumpOp(op); NEXT2;
+        NEXT2; default: dumpOp(pc-1, op); NEXT2;
     }
 }
 
 void runStack(const char *x) {
     pc = 0;
-    next:
+    stkNext:
     u = x[pc++];
 
     switch(u) {
-    case  0: return;
-    case  ' ':
-    NCASE '!': printf("%c",u);
-    NCASE '"': printf("%c",u);
-    NCASE '#': t=S0; PS(t);
-    NCASE '$': t=S0; S0=S1; S1=t;
-    NCASE '%': t=S1; PS(t);
-    NCASE '&': printf("%c",u);
-    NCASE '\'': D1;
-    NCASE '(': if (PP==0) { while (x[pc++]!=')') {} }
-    NCASE ')': printf("%c",u);
-    NCASE '*': S1*=S0; D1;
-    NCASE '+': S1+=S0; D1;
-    NCASE ',': printf("%c",u);
-    NCASE '-': S1-=S0; D1;
-    NCASE '.': printf(" %jd",PP);
-    NCASE '/': S1/=S0; D1;
-    NCASE '0': case '1': case '2': case '3': case  '4': case '5': case '6': case '7':
-    case  '8': case '9': PS(u-'0'); while (BTW(x[pc],'0','9')) { S0=(S0*10)+(x[pc++]-'0'); }
-    NCASE '<': S1=(S1<S0)?-1:0;  D1;
-    NCASE '=': S1=(S1==S0)?-1:0; D1;
-    NCASE '>': S1=(S1>S0)?-1:0;  D1;
-    NCASE 'I': PS(L0);
-    NCASE 'N': printf("\n");
-    NCASE 'Q': exit(0);
-    NCASE 'T': PS(clock());
-    NCASE '[': lsp+=3; L0=PP; L1=PP; L2=(cell_t)pc;
-    NCASE '\\': if (0<sp) sp--;
-    NCASE ']': if (++L0<L1) { pc=(int)L2; } else { lsp-=3; }
-    NCASE 'd': --S0;
-    NCASE 'i': ++S0;
-    NCASE 'l': u=x[pc++]; PS(&loc[u-'0']);
-    NCASE 'r': u=x[pc++]; PS(reg[u-'A']);
-    NCASE 's': u=x[pc++]; reg[u-'A']=PP;
-    NCASE '{': lsp+=3; L2=(cell_t)pc;
-    NCASE '}': if (PP) { pc=(int)L2; } else { lsp -=3; }
+        case  0: return;
+        case  ' ':
+        NCASE '!': printf("%c",u);
+        NCASE '"': printf("%c",u);
+        NCASE '#': t=S0; PS(t);
+        NCASE '$': t=S0; S0=S1; S1=t;
+        NCASE '%': t=S1; PS(t);
+        NCASE '&': printf("%c",u);
+        NCASE '\'': D1;
+        NCASE '(': if (PP==0) { while (x[pc++]!=')') {} }
+        NCASE ')': printf("%c",u);
+        NCASE '*': S1*=S0; D1;
+        NCASE '+': S1+=S0; D1;
+        NCASE ',': printf("%c",u);
+        NCASE '-': S1-=S0; D1;
+        NCASE '.': printf(" %jd",PP);
+        NCASE '/': S1/=S0; D1;
+        NCASE '0': case '1': case '2': case '3': case  '4': case '5': case '6': case '7':
+        case  '8': case '9': PS(u-'0'); while (BTW(x[pc],'0','9')) { S0=(S0*10)+(x[pc++]-'0'); }
+        NCASE '<': S1=(S1<S0)?-1:0;  D1;
+        NCASE '=': S1=(S1==S0)?-1:0; D1;
+        NCASE '>': S1=(S1>S0)?-1:0;  D1;
+        NCASE 'I': PS(L0);
+        NCASE 'N': printf("\n");
+        NCASE 'Q': exit(0);
+        NCASE 'T': PS(clock());
+        NCASE '[': lsp+=3; L0=PP; L1=PP; L2=(cell_t)pc;
+        NCASE '\\': if (0<sp) sp--;
+        NCASE ']': if (++L0<L1) { pc=(int)L2; } else { lsp-=3; }
+        NCASE 'd': --S0;
+        NCASE 'i': ++S0;
+        NCASE 'l': u=x[pc++]; PS(&loc[u-'0']);
+        NCASE 'r': u=x[pc++]; PS(reg[u-'A']);
+        NCASE 's': u=x[pc++]; reg[u-'A']=PP;
+        NCASE '{': lsp+=3; L2=(cell_t)pc;
+        NCASE '}': if (PP) { pc=(int)L2; } else { lsp -=3; }
     }
 }
 
@@ -193,36 +196,39 @@ void pw() {
         PS(w[l++]-'0');
         while (BTW(w[l],'0','9')) { S0=(S0*10)+(w[l++]-'0'); }
         if ((w[l]=='s') && BTW(w[l+1],'A','Z')) { c01_l1('m', w[l+1], PP32); }
-        if ((w[l]=='s') && BTW(w[l+1],'0','9')) { c01_l1('M', w[l+1], PP32); }
+        else if ((w[l]=='s') && BTW(w[l+1],'0','9')) { c01_l1('M', w[l+1], PP32); }
         else { c0_l1('n', PP32); }
         return;
     }
 
     switch (w[0]) {
         case  '+': c0123( w[0], w[1], w[2], w[3]);
-        RCASE '*': c0123( w[0], w[1], w[2], w[3]);
-        RCASE '-': c0123( w[0], w[1], w[2], w[3]);
-        RCASE '/': c0123( w[0], w[1], w[2], w[3]);
-        RCASE 'r': if (!doRegR()) { doId(); }        // RegRead
-        RCASE 's': if (!doRegS()) { doId(); }        // RegSet
-        RCASE 't': c01( w[0], w[1]);    // Timer
-        RCASE '.': c01( w[0], w[1] );
-        RCASE ',': c01( w[0], w[1] );
-        RCASE ']': c0(  w[0] );
-        RCASE '[': c0(  w[0] );
+        RETCASE '*': c0123( w[0], w[1], w[2], w[3]);
+        RETCASE '-': c0123( w[0], w[1], w[2], w[3]);
+        RETCASE '/': c0123( w[0], w[1], w[2], w[3]);
+        RETCASE 'r': if (!doRegR()) { doId(); }        // RegRead
+        RETCASE 's': if (!doRegS()) { doId(); }        // RegSet
+        RETCASE 't': c01( w[0], w[1]);    // Timer
+        RETCASE '.': c01( w[0], w[1] );
+        RETCASE ',': c01( w[0], w[1] );
+        RETCASE ']': c0(  w[0] );
+        RETCASE '[': c0(  w[0] );
         return; default: printf("unknown [%d]", w[0]);
     }
 }
 
 void cc(char *x) {
+    here = (here<1) ? 1 : here;
     in = x;
     while (nextWd()) {
         if (strEq(w,"//")) return;
         pw();
+        code[0].i64 = here;
     }
 }
 
 int loop(FILE *fp) {
+    int lh = here;
     char x[128], ok = 0;
     if (fp) {
         if (fgets(x, 128, fp) != x) { return 0; }
@@ -238,7 +244,8 @@ int main() {
         if (!loop(fp)) { 
             fclose(fp);
             fp=NULL;
-            runReg(0);
+            for (int i=0; i<here; i++) { dumpOp(i, &code[i]); }
+            runReg(1);
         }
     }
 
