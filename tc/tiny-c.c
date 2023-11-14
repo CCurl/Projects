2187d9fee1eaec11fdfaacd83c07d389d69d6efc
Christopher Curl <ccurl@mmm.com>
2023-11-14 17:13:40 -0500

    start adding functions

--- tc/tiny-c.c
+++ tc/tiny-c.c
@@ -1,7 +1,7 @@
 /* file: "tc.c" */
 /* originally from http://www.iro.umontreal.ca/~felipe/IFT2030-Automne2002/Complements/tinyc.c */
 /* Copyright (C) 2001 by Marc Feeley, All Rights Reserved. */
-/* enhanced by Chris Curl */
+/* Modified and enhanced by Chris Curl */
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -39,25 +39,6 @@
  *  <int> ::= <an_unsigned_decimal_integer>
  *  <func> ::= <id> "(" ")" ";"
  *
- * Here are a few invocations of the compiler:
- *
- * % echo "a=b=c=2<3;" | ./a.out
- * a = 1
- * b = 1
- * c = 1
- * % echo "{ i=1; while (i<100) i=i+i; }" | ./a.out
- * i = 128
- * % echo "{ i=125; j=100; while (i-j) if (i<j) j=j-i; else i=i-j; }" | ./a.out
- * i = 25
- * j = 25
- * % echo "{ i=1; do i=i+10; while (i<50); }" | ./a.out
- * i = 51
- * % echo "{ i=1; while ((i=i+10)<50) ; }" | ./a.out
- * i = 51
- * % echo "{ i=7; if (i<5) x=1; if (i<10) y=2; }" | ./a.out
- * i = 7
- * y = 2
- *
  * The compiler does a minimal amount of error checking to help
  * highlight the structure of the compiler.
  */
@@ -69,7 +50,7 @@
 enum { DO_SYM, ELSE_SYM, IF_SYM, WHILE_SYM, VOID_SYM, INT_SYM, //  0->5
        LBRA, RBRA, LPAR, RPAR,                                 //  6->9
        PLUS, MINUS, STAR, SLASH, LESS, GRT, SEMI, EQUAL,       // 10->17
-       INT, ID, EOI };                                         // 18->20
+       INT, ID, EOI, FUNC_SYM };                               // 18->20
 
 char *words[] = { "do", "else", "if", "while", "void", "int", NULL };
 
@@ -84,7 +65,7 @@ void syntax_error() { error("-syntax error-"); }
 void next_ch() {
     if (input_fp) { ch = fgetc(input_fp); }
     else { ch = getchar(); }
-    //printf("(%d)", ch);
+    // if (BTWI(ch,32,126)) { printf("%c", ch); } else { printf("(%d)", ch); }
 }
 
 int isAlpha(int ch) {
@@ -127,30 +108,33 @@ void next_sym() {
             id_name[i] = '\0';
             sym = 0;
             while (words[sym] != NULL && strcmp(words[sym], id_name) != 0) { sym++; }
-            // printf("-ID:%s:%d-", id_name, sym);
             if (words[sym] == NULL) {
               sym = ID;
-              // if (id_name[1] == '\0') { sym = ID; } // a-z only for now
-              // else { message("-id-len-"); syntax_error(); }
+              if (ch=='(') {
+                  next_ch();
+                  if (ch==')') { sym = FUNC_SYM; next_ch(); }
+                  else { syntax_error(); }
+              }
             }
         } else { message("-ch-"); syntax_error(); }
         break;
     }
-    //fsprintf("-[sym:%d]-",sym);
 }
 
 /*---------------------------------------------------------------------------*/
 /* Parser. */
 
-enum { VAR, CST, ADD, SUB, MUL, DIV, LT, GT, SET,
+enum { VAR, CST, ADD, SUB, MUL, DIV, LT, GT, SET, FUNC_CALL,
        IF1, IF2, WHILE, DO, EMPTY, SEQ, EXPR, PROG };
 
+#define MAX_NODES 1000
 struct node_s { int kind; struct node_s *o1, *o2, *o3; int val; };
 typedef struct node_s node;
 int num_nodes = 0;
-node nodes[1000];
+node nodes[MAX_NODES];
 
 node *new_node(int k) {
+    if (MAX_NODES <= num_nodes) { error(""); }
     node *x = &nodes[num_nodes++];
     x->kind = k;
     return x;
@@ -163,7 +147,10 @@ node *gen(int k, node *o1, node *o2) {
 }
 
 void expect_sym(int exp) {
-    if (sym != exp) { printf("-expected[%d],not[%d]-", exp, sym); syntax_error(); }
+    if (sym != exp) {
+        printf("-expected symbol[%d],not[%d]-", exp, sym);
+        syntax_error();
+    }
     next_sym();
 }
 
@@ -172,9 +159,15 @@ node *paren_expr(); /* forward declaration */
 /* <term> ::= <id> | <int> | <paren_expr> */
 node *term() {
   node *x;
-  if (sym == ID) { x=new_node(VAR); x->val=id_name[0]-'a'; next_sym(); }
-  else if (sym == INT) { x=new_node(CST); x->val=int_val; next_sym(); }
-  else x = paren_expr();
+  if (sym == ID) {
+      x=new_node(VAR);
+      x->val=id_name[0]-'a'; // Update this for longer names
+      next_sym();
+  } else if (sym == INT) {
+      x=new_node(CST);
+      x->val=int_val;
+      next_sym();
+  } else x = paren_expr();
   return x;
 }
 
@@ -209,9 +202,12 @@ node *test() {
 /* <expr> ::= <test> | <id> "=" <expr> */
 node *expr() {
   node *x;
-  if (sym != ID) return test();
+  if (sym != ID) { return test(); }
   x = test();
-  if ((x->kind==VAR) && (sym==EQUAL)) { next_sym(); return gen(SET, x, expr()); }
+  if ((x->kind==VAR) && (sym==EQUAL)) {
+      next_sym();
+      return gen(SET, x, expr());
+  }
   return x;
 }
 
@@ -241,6 +237,13 @@ node *statement() {
      next_sym();
      x->o1=paren_expr();
      x->o2=statement();
+  } else if (sym == FUNC_SYM) { /* <id> "();" */
+      x=new_node(FUNC_CALL);
+      printf("-call %s()-", id_name);
+      // TODO: call the function
+      x->val = 12345;
+      next_sym();
+      expect_sym(SEMI);
   } else if (sym == DO_SYM) { /* "do" <statement> "while" <paren_expr> ";" */
       x = new_node(DO);
       next_sym();
@@ -251,7 +254,7 @@ node *statement() {
   } else if (sym == SEMI) { /* ";" */
       x = new_node(EMPTY);
       next_sym();
-  } else if (sym == LBRA) { /* "{" { <statement> } "}" */
+  } else if (sym == LBRA) { /* "{" <statement> "}" */
       x = new_node(EMPTY);
       next_sym();
       while (sym != RBRA) {
@@ -274,10 +277,9 @@ node *defs(node *st) {
     if (sym==EOI) { break; }
     if (sym==LBRA) { break; }
     if (sym==VOID_SYM) {
-      next_sym(); expect_sym(ID);
-      printf("-FUNC %s-", id_name);
-      expect_sym(LPAR);
-      expect_sym(RPAR);
+      next_sym(); expect_sym(FUNC_SYM);
+      printf("-def %s()-", id_name);
+      // TODO: Add the function
       expect_sym(LBRA);
       x = new_node(EMPTY);
       while (sym != RBRA) {
@@ -312,86 +314,115 @@ node *program() {
 /*---------------------------------------------------------------------------*/
 /* Code generator. */
 
-enum { IFETCH, ISTORE, IPUSH, IDROP, IADD, ISUB, IMUL, IDIV, ILT, IGT, JZ, JNZ, JMP, HALT };
+enum { IFETCH, ISTORE, IPUSH, IDROP, IADD, ISUB, IMUL, IDIV, ILT, IGT, JZ, JNZ, JMP, ICALL, IRET, HALT };
 
 typedef char code;
-code object[1000], *here = object;
+code vm[1000], *here = vm;
 
 void g(code c) { *here++ = c; } /* missing overflow check */
+void g4(int n) {
+    g(n & 0xff); n=(n >> 8);
+    g(n & 0xff); n=(n >> 8);
+    g(n & 0xff); n=(n >> 8);
+    g(n & 0xff);
+}
+void g2(int n) {
+    g(n & 0xff); n=(n >> 8);
+    g(n & 0xff);
+}
 code *hole() { return here++; }
 void fix(code *src, code *dst) { *src = dst-src; } /* missing overflow check */
 
 void c(node *x) {
-  code *p1, *p2;
-  switch (x->kind)
-    { case VAR  : g(IFETCH); g(x->val); break;
-      case CST  : g(IPUSH);  g(x->val); break;
-      case ADD  : c(x->o1);  c(x->o2); g(IADD); break;
-      case MUL  : c(x->o1);  c(x->o2); g(IMUL); break;
-      case SUB  : c(x->o1);  c(x->o2); g(ISUB); break;
-      case DIV  : c(x->o1);  c(x->o2); g(IDIV); break;
-      case LT   : c(x->o1);  c(x->o2); g(ILT); break;
-      case GT   : c(x->o1);  c(x->o2); g(IGT); break;
-      case SET  : c(x->o2);  g(ISTORE); g(x->o1->val); break;
-      case IF1  : c(x->o1);  g(JZ); p1=hole(); c(x->o2); fix(p1,here); break;
-      case IF2  : c(x->o1);  g(JZ); p1=hole(); c(x->o2); g(JMP); p2=hole();
-                  fix(p1,here); c(x->o3); fix(p2,here); break;
-      case WHILE: p1=here; c(x->o1); g(JZ); p2=hole(); c(x->o2);
-                  g(JMP); fix(hole(),p1); fix(p2,here); break;
-      case DO   : p1=here; c(x->o1); c(x->o2); g(JNZ); fix(hole(),p1); break;
-      case EMPTY: break;
-      case SEQ  : c(x->o1); c(x->o2); break;
-      case EXPR : c(x->o1); g(IDROP); break;
-      case PROG : c(x->o1); g(HALT);  break;
+    code *p1, *p2;
+    switch (x->kind) {
+        case VAR  : g(IFETCH); g2(x->val); break;
+        case CST  : g(IPUSH);  g4(x->val); break;
+        case ADD  : c(x->o1);  c(x->o2); g(IADD); break;
+        case MUL  : c(x->o1);  c(x->o2); g(IMUL); break;
+        case SUB  : c(x->o1);  c(x->o2); g(ISUB); break;
+        case DIV  : c(x->o1);  c(x->o2); g(IDIV); break;
+        case LT   : c(x->o1);  c(x->o2); g(ILT); break;
+        case GT   : c(x->o1);  c(x->o2); g(IGT); break;
+        case SET  : c(x->o2);  g(ISTORE); g2(x->o1->val); break;
+        case IF1  : c(x->o1);  g(JZ); p1=hole(); c(x->o2); fix(p1,here); break;
+        case IF2  : c(x->o1);  g(JZ); p1=hole(); c(x->o2); g(JMP); p2=hole();
+                    fix(p1,here); c(x->o3); fix(p2,here); break;
+        case WHILE: p1=here; c(x->o1); g(JZ); p2=hole(); c(x->o2);
+                    g(JMP); fix(hole(),p1); fix(p2,here); break;
+        case DO   : p1=here; c(x->o1); c(x->o2); g(JNZ); fix(hole(),p1); break;
+        case EMPTY: break;
+        case SEQ  : c(x->o1); c(x->o2); break;
+        case EXPR : c(x->o1); g(IDROP); break;
+        case PROG : c(x->o1); g(HALT);  break;
+        case FUNC_CALL: g(ICALL); g2(x->val); break;
     }
 }
 
 /*---------------------------------------------------------------------------*/
 /* Virtual machine. */
 
-int globals[26], sp;
+int globals[26], sp, rsp;
 
 #define ACASE    goto again; case
 #define TOS      st[sp]
 #define NOS      st[sp-1]
 
+int f4(unsigned char *pc) {
+    int x=*(pc+3);
+    x = (x<<8)|*(pc+2);
+    x = (x<<8)|*(pc+1);
+    x = (x<<8)|*(pc+0);
+    return (int)x;
+}
+
+int f2(code *pc) {
+    int x=*(pc+1);
+    x = (x<<8)|*(pc+0);
+    return x;
+}
+
 void run(code *pc) {
-  long st[1000];
-  again:
-  switch (*pc++) {
-      case IFETCH : st[++sp] = globals[*pc++];
-      ACASE ISTORE: globals[*pc++] = st[sp];
-      ACASE IPUSH : st[++sp] = *pc++;
-      ACASE IDROP : --sp;
-      ACASE IADD  : NOS += TOS; --sp;
-      ACASE ISUB  : NOS -= TOS; --sp;
-      ACASE IMUL  : NOS *= TOS; --sp;
-      ACASE IDIV  : NOS /= TOS; --sp;
-      ACASE ILT   : NOS =  NOS<TOS; --sp;
-      ACASE IGT   : NOS =  NOS>TOS; --sp;
-      ACASE JMP   : pc += *pc;
-      ACASE JZ    : if (st[sp--] == 0) pc += *pc; else pc++;
-      ACASE JNZ   : if (st[sp--] != 0) pc += *pc; else pc++;
-      ACASE HALT  : return;
-  }
+    long st[1000];
+    code *rst[1000];
+    again:
+    switch (*pc++) {
+        case  IFETCH: st[++sp] = globals[f2(pc)]; pc +=2;
+        ACASE ISTORE: globals[f2(pc)] = st[sp]; pc += 2;
+        ACASE IPUSH : st[++sp] = f4(pc); pc += 4;
+        ACASE IDROP : --sp;
+        ACASE IADD  : NOS += TOS; --sp;
+        ACASE ISUB  : NOS -= TOS; --sp;
+        ACASE IMUL  : NOS *= TOS; --sp;
+        ACASE IDIV  : NOS /= TOS; --sp;
+        ACASE ILT   : NOS =  (NOS<TOS)?1:0; --sp;
+        ACASE IGT   : NOS =  (NOS>TOS)?1:0; --sp;
+        ACASE JMP   : pc += *pc;
+        ACASE JZ    : if (st[sp--] == 0) pc += *pc; else pc++;
+        ACASE JNZ   : if (st[sp--] != 0) pc += *pc; else pc++;
+        ACASE ICALL : /* rst[rsp++] = pc+2; pc=f2(pc); */
+                        printf("-run:call %d-\n", f2(pc)); pc += 2; 
+        ACASE IRET : if (rst) { pc=rst[--rsp]; } else { error("-return-"); }
+        ACASE HALT  : return;
+    }
 }
 
 /*---------------------------------------------------------------------------*/
 /* Main program. */
 
 int main(int argc, char *argv[]) {
-  if (argc>1) { input_fp = fopen(argv[1], "rt"); }
-  c(program());
+    if (argc>1) { input_fp = fopen(argv[1], "rt"); }
+    c(program());
 
-  printf("(nodes: %d, ", num_nodes);
-  printf("code: %d bytes)\n", (int)(here-&object[0]));
+    printf("(nodes: %d, ", num_nodes);
+    printf("code: %d bytes)\n", (int)(here-&vm[0]));
 
-  sp=0;
-  for (int i=0; i<26; i++) { globals[i] = 0; }
-  run(object);
-  for (int i=0; i<26; i++) {
-    if (globals[i] != 0) printf("%c = %d\n", 'a'+i, globals[i]);
-  }
-  if (sp) { error("-stack not empty-"); }
-  return 0;
+    sp=rsp=0;
+    for (int i=0; i<26; i++) { globals[i] = 0; }
+    run(vm);
+    for (int i=0; i<26; i++) {
+        if (globals[i] != 0) printf("%c = %d\n", 'a'+i, globals[i]);
+    }
+    if (sp) { error("-stack not empty-"); }
+    return 0;
 }
