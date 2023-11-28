21a84495e7ad7bdde76c0e92dfea8705438484a8
Chris Curl <curlbud-github@yahoo.com>
2023-11-28 02:29:42 -0500

    a little more

--- c3-fasm/c3.s
+++ c3-fasm/c3.s
@@ -4,78 +4,105 @@ segment readable executable
 entry cold
 
 ; ------------------------------------------------------------------------------
+CODE_SZ equ 65536
+RSP     equ r13
+PC      equ r14
+TOS     equ r15
+HERE    equ rbp
+
+; ------------------------------------------------------------------------------
 macro dPUSH val {
-    add r14, 8
-    mov [r14], QWORD val
+    push TOS
+    mov TOS, QWORD val
 }
 
 ; ------------------------------------------------------------------------------
 macro dPOP val {
-    mov val, QWORD [r14]
-    sub r14, 8
+    mov val, TOS
+    pop TOS
 }
 
 ; ------------------------------------------------------------------------------
 macro rPUSH val {
-    add r11, 8
-    mov [r11], qword val
+    add RSP, 8
+    mov [RSP], qword val
 }
 
 ; ------------------------------------------------------------------------------
 macro rPOP val {
-    mov val, [r11]
-    sub r11, 8
+    mov val, [RSP]
+    sub RSP, 8
 }
 
 ; ------------------------------------------------------------------------------
 macro CComma val {
-    mov [r13], byte val
-    inc r13
+    mov [rbp], byte val
+    inc rbp
 }
 
 ; ------------------------------------------------------------------------------
 macro Comma val {
-    mov [r13], qword val
-    add r13, 8
+    mov [rbp], qword val
+    add rbp, 8
 }
 
 ; ------------------------------------------------------------------------------
 macro nextByte val {
-    movzx val, byte [r15]
-    inc r15
+    movzx val, byte [PC]
+    inc PC
 }
 
 ; ------------------------------------------------------------------------------
 macro nextCell val {
-    mov val, qword [r15]
-    add r15, 8
+    mov val, qword [PC]
+    add PC, 8
+}
+
+; ------------------------------------------------------------------------------
+macro mLit1 val {
+    CComma 1
+    CComma val
+}
+
+; ------------------------------------------------------------------------------
+macro mLit val {
+    CComma 2
+    Comma val
 }
 
 ; ------------------------------------------------------------------------------
 ; ------------------------------------------------------------------------------
 ; ------------------------------------------------------------------------------
 cold:
-    lea r11, [rstk]                              ; r11: Return STACK
-    lea r12, [code]                              ; r12: LAST: end of the code
-    add r12, 65536
-    lea r13, [code]                              ; r13: HERE: start of the code
-    lea r14, [stk]                               ; r14: STACK
-    lea r15, [code]                              ; r15: PC: start at beginning of the code
+    lea RSP, [rstk]                             ; Start of return STACK
+    lea rax, [buf2]                             ; LAST: end of the code
+    mov [last], rax
+    lea PC, [code]                              ; PC: start at beginning of the code
+    mov HERE, PC                                ; HERE: start of code
 
-    ; testing
-    ;lea rax, [hi]
-    ;dPUSH rax
-    ;mov rax, 5
-    ;dPUSH rax
-    ;call stype
+    ; Some overall testing ...
+    lea rsi, [hi]
+    mov [toIn], rsi
+    call nwTest
+    call nwTest
+    call nwTest
     ; jmp bye
 
-    CComma 2                  ; lit
+    ; The test Byte Code ...
     lea rax, [c3]             ; "c3: "
-    Comma rax
-    CComma 1                  ; lit1
-    CComma 5                  ; 5
+    mLit rax                  ; lit
+    mLit1 5                   ; lit1 5 (len)
     CComma 8                  ; type
+    mLit1 'M'                 ; lit1 'A'
+    mLit1 5                   ; lit1 5
+    CComma 11                 ; sub
+    CComma 9                  ; emit
+    mLit1 'A'                 ; lit1 'A'
+    mLit1 5                   ; lit1 5
+    CComma 10                 ; add
+    CComma 9                  ; emit
+    mLit1 10                  ; lit1 '\n'
+    CComma 9                  ; emit
 
     CComma 0                  ; bye
 
@@ -85,6 +112,66 @@ next: nextByte rcx
     jmp rax
 
 ; ------------------------------------------------------------------------------
+nwTest: call nextWord
+    call stype
+    mov al, ','
+    call semit
+    ret
+
+; ------------------------------------------------------------------------------
+; out: RSI: the word, RDX: length
+; ------------------------------------------------------------------------------
+nextWord: cld
+    lea rdi, [wd]
+    mov rsi, qword [toIn]
+    xor rdx, rdx
+    cmp byte [esi], 0
+    je .9
+.1: lodsb               ; Skip WS
+    test al, al
+    jz .9
+    cmp al, 33
+    jl .1
+.2: stosb               ; Build the word
+    inc rdx             ; RDX: len
+    lodsb
+    cmp al, 32
+    jg .2
+.9: xor al, al          ; Done
+    stosb
+    mov [toIn], esi     ; word is in RSI, len in RDX
+    lea rsi, [wd]
+    ret
+
+; ------------------------------------------------------------------------------
+; Dict Entry: 32 bytes ... [xt:8] [flags:1] [len:1] [name:21] [null:1]
+; Word is in RSI, len is in RDX
+; ------------------------------------------------------------------------------
+addToDict:
+    test dl, dl
+    jz .r
+    push HERE
+    mov rax, HERE
+    mov HERE, [last]
+    sub HERE, 32
+    mov [last], HERE
+    Comma rax           ; XT
+    CComma 0            ; Flags
+    cmp dl, 21          ; Check len
+    jle .l
+    mov dl, 21          ; trunc to 21
+.l: CComma dl           ; Len
+.s: lodsb               ; Name
+    CComma al
+    dec dl
+    jnz .s
+    CComma 0            ; NULL
+    pop HERE
+.r: ret
+
+; ------------------------------------------------------------------------------
+; The VM opcodes
+; ------------------------------------------------------------------------------
 lit1: nextByte rax
     dPUSH rax
     jmp next
@@ -93,63 +180,80 @@ lit: nextCell rax
     dPUSH rax
     jmp next
 
-exit: rPOP r15
+exit: rPOP PC
     jmp next
 
 fcall: nextCell rax
-    rPUSH r15
-    mov r15, rax
+    rPUSH PC
+    mov PC, rax
     jmp next
 
 fjmp: nextCell rax
-    mov r15, rax
+    mov PC, rax
     jmp next
 
 fjmpz: dPOP rax
     test rax, rax
     jz fjmp
-    add r15, 8
+    add PC, 8
     jmp next
 
 fjmpnz: POP rax
     test rax, rax
     jnz fjmp
-    add r15, 8
+    add PC, 8
     jmp next
 
-emit: dPOP rax
-    lea rsi, [buf]
+myAdd: dPOP rax
+    add TOS, rax
+    jmp next
+
+mySub: dPOP rax
+    sub TOS, rax
+    jmp next
+
+semit:              ; The char to EMIT is in al
+    lea rsi, [buf2]
     mov [rsi], al
     mov rdx, 1      ; len=1
     mov rdi, 1      ; stdout
     mov rax, 1      ; sys_write
     syscall
-    jmp next
-
-stype: dPOP rdx     ; len
-    dPOP rsi        ; string
-    mov rdi, 1      ; stdout
-    mov rax, 1      ; sys_write
-    ; push r11 r12 r13 r14 r15
-    syscall
-    ; pop r15 r14 r13 r12 r11
     ret
 
-type: call stype
+emit: dPOP rax
+    call semit
+    jmp next
+
+stype:              ; The string is in RSI, the len is in RDX
+    mov rdi, 1      ; stdout
+    mov rax, 1      ; sys_write
+    syscall
+    ret
+
+type: dPOP rdx      ; len
+    dPOP rsi        ; string
+    call stype
     jmp next
 
 bye: xor rdi, rdi                                ; exit code 0
     mov rax, 60                                  ; sys_exit
     syscall
+    ret
 
 ; ------------------------------------------------------------------------------
 segment readable writable
 
-hi:   db 'hi. ', 10, 0
+hi:   db 'test 123', 10, 0
 c3:   db 'c3: ', 10, 0
-buf:  rb 256
-tbl:  dq bye, lit1, lit, exit, fcall, fjmp, fjmpz, fjmpnz
-      dq type, emit
-stk:  dq 256
-rstk: dq 256
-code: rb 1000
+tbl:  dq bye, lit1, lit, exit, fcall, fjmp, fjmpz, fjmpnz   ;  0 ->  7
+      dq type, emit                                         ;  8 ->  9
+      dq myAdd, mySub                                       ; 10 -> 11
+; here: rq 1
+toIn: rq 1
+last: rq 1
+rstk: rq 64
+wd:   rb 32
+tib:  rb 256
+code: rb CODE_SZ
+buf2: rb 16
