285ac5e03565583c7a273302a68a1c3cf9603791
Chris Curl <A15Q7ZZ@3mhealth.com>
2024-02-02 09:59:43 -0500

    update ccc.bat

--- batch/ccc.bat
+++ batch/ccc.bat
@@ -5,52 +5,47 @@ if '--%1--' equ '--/?--'    goto help
 if '--%1--' equ '--?--'     goto help
 if '--%1--' equ '--help--'  goto help
 if '--%1--' equ '--/help--' goto help
-if '--%1--' equ '--c3--'    goto c3
-if '--%1--' equ '--code--'  goto code
-if '--%1--' equ '--lg--'    goto lg
-if '--%1--' equ '--proj--'  goto proj
-if '--%1--' equ '--pull--'  goto pull
+if '--%1--' equ '--c3--' (
+	cd \code\c3
+	goto endCD
+)
+if '--%1--' equ '--code--' (
+	cd \code
+	goto end
+)
+if '--%1--' equ '--lg--' (
+	lazygit
+	goto end
+)
+if '--%1--' equ '--proj--' (
+	cd \code\projects
+	goto endCD
+)
+if '--%1--' equ '--pull--' (
+	git pull
+	goto end
+)
 
 echo ccc: command '%1' not found.
 echo;
 goto help
 
-:c3
-cd \code\c3
-goto endCD
-
-:proj
-cd \code\projects
-goto endCD
-
-:code
-cd \code
-goto endCD
-
-:pull
-git pull
-goto end
-
-:lg
-lazygit
-goto end
-
 :endCD
-cd
-if '--%2--' equ '--pull--' goto pull
-if '--%2--' equ '--lg--' goto lg
+echo Current folder: %CD%
+if '--%2--' equ '--pull--' ( git pull )
+if '--%2--' equ '--lg--' ( lazygit )
 goto end
 
 :help
 echo Usage: ccc [command], where [command] is one of:
 echo;
-echo   c3:    cd to my c3 folder
-echo   code:  cd to my code folder
-echo   lg:    execute 'lazygit'
-echo   proj:  cd to my Projects folder
-echo   pull:  execute 'git pull'
+echo   c3:    CD to my c3 folder
+echo   code:  CD to my code folder
+echo   lg:    Execute 'lazygit'
+echo   proj:  CD to my Projects folder
+echo   pull:  Execute 'git pull'
 echo   NOTE:  'pull' or 'lg' can also be provided as a second
-echo            parameter for the commands that change folders
+echo            parameter for the commands that CD to git folders
 echo            e.g. 'ccc c3 pull' or 'ccc proj lg'
 
 :end
