a38c4aea30df5e73263250ac70caf9a2889e331a
Chris Curl <ccurl@solventum.com>
2025-07-11 12:12:13 -0400

    more

--- pg/boot.lsp
+++ pg/boot.lsp
@@ -1,3 +1,6 @@
-( 1 2 3 4 5 ( true nil ) ( 6 7 ) )
-( 22 ( 66 false 77 ) apple Banana )
+( 1 2 3 4 5 ( true nil ) ( 6 7 ) "hi there" )
+( 22 
+	( 66 false 77 )
+	apple Banana 'another one'
+)
 12345 
--- pg/lsp.cpp
+++ pg/lsp.cpp
@@ -24,9 +24,6 @@
 #define BCASE break; case
 #define SNODE struct NODE_S
 
-enum { SYM_UNK, SYM_LPAR, SYM_RPAR, SYM_IF, SYM_T, SYM_F, SYM_NIL,
-	 SYM_NUM, SYM_STRING, SYM_SYMBOL, SYM_EOI };
-	 
 typedef unsigned long ulong;
 typedef union {
 	long nVal;
@@ -35,15 +32,15 @@ typedef union {
 } ATOM_T;
 typedef SNODE { SNODE *next; SNODE *child; int type; ATOM_T val; } NODE_T;
 
-// These line up with the enums of SYMBOLS
-const char *sym_words[] = { "", "(", ")", "if", "true", "false", "nil", NULL };
-
 int ch = ' ', sym, state = 0;
 char sym_name[32];
 char tib[1024], *toIn, tok[64];
 NODE_T object[NUM_OBJECTS], *freeList;
 int len, tokType;
 long theNum;
+
+/*---------------------------------------------------------------------------*/
+/* Subs. */
 
 void zType(const char *str) { fputs(str, stdout); }
 void message(const char *msg, int addLF) { fprintf(stderr, "%s%s", msg, addLF ? "\n" : ""); }
@@ -53,33 +50,56 @@ void syntax_error() { error("syntax error"); }
 int isAlpha(int ch) { return BTWI(ch,'A','Z') || BTWI(ch,'a','z'); }
 int isNum(int ch) { return BTWI(ch,'0','9'); }
 int isAlphaNum(int ch) { return isAlpha(ch) || isNum(ch); }
-void next_ch() { ch = (*toIn) ? *(toIn++) : 0; }
 
 NODE_T *getNext(NODE_T *obj) { return obj->next; }
 
-NODE_T *myAlloc(int type, NODE_T *next) {
-	NODE_T *obj = NULL;
+NODE_T* myAlloc(int type, NODE_T* next) {
+	NODE_T* obj = NULL;
 	if (freeList == NULL) { error("Empty!"); }
 	else {
 		obj = freeList;
 		freeList = getNext(obj);
 		obj->type = type;
 		obj->next = next;
+		obj->child = NULL;
 	}
 	return obj;
-}	
+}
 
-void myFree(NODE_T *obj) {
+void myFree(NODE_T* obj) {
 	obj->next = freeList;
 	freeList = obj;
 }
 
-long isNum(char *wd) {
+/*---------------------------------------------------------------------------*/
+/* Symbol stream. */
+
+enum {
+	SYM_UNK, SYM_LPAR, SYM_RPAR, SYM_IF, SYM_TRUE, SYM_FALSE, SYM_NIL,
+	SYM_NUMBER, SYM_STRING, SYM_ENUM, SYM_EOI
+};
+
+FILE *inputFp;
+char *getInput() {
+	restart:
+	if (inputFp == 0) { zType("\nlsp>"); }
+	if (tib == fgets(tib, sizeof(tib), inputFp ? inputFp : stdin)) {
+		tib[strlen(tib) - 1] = 0;
+		return tib;
+	}
+	else {
+		if (inputFp) { fclose(inputFp); }
+		inputFp = NULL;
+		goto restart;
+	}
+}
+
+long isNum(char *w) {
 	int isNeg = 0;
 	long x = 0;
-	if (*wd == '-') { ++wd; isNeg = 1; }
-	while (*wd) {
-		char c = *(wd++);
+	if (*w == '-') { ++w; isNeg = 1; }
+	while (*w) {
+		char c = *(w++);
 		if (!BTWI(c,'0','9')) { return 0; }
 		x = (x*10) + (c-'0');
 	}
@@ -87,14 +107,21 @@ long isNum(char *wd) {
 	return 1;
 }
 
+int isEnum(char *w) {
+	if (BTWI(w[0], 'A', 'Z')) { return 1; }
+	if (BTWI(w[0], 'a', 'z')) { return 1; }
+	return 0;
+}
+
 int symSub() {
+	restart:
 	while (*toIn && (*toIn < 33)) { ++toIn; }
-	if (*toIn == 0)   { return SYM_EOI; }
+	if (*toIn == 0) { toIn = getInput(); goto restart; }
 	
 	len = 0;
-	if (*toIn == '"') {
-		++toIn;
-		while (*toIn && (*toIn != '"')) { tok[len++] = *(toIn++); }
+	if ((*toIn == '"') || (*toIn == '\'')) {
+		char c = *(toIn++);
+		while (*toIn && (*toIn != c)) { tok[len++] = *(toIn++); }
 		if (*toIn) { ++toIn; }
 		tok[len] = 0;
 		return SYM_STRING;
@@ -103,79 +130,66 @@ int symSub() {
 	while (*toIn && (*toIn > 32)) { tok[len++] = *(toIn++); }
 	tok[len] = 0;
 	
-	if (isNum(tok)) { return SYM_NUM; }
-	for (int i = 0; sym_words[i]; i++ ) {
-		if (strcmp(sym_words[i], tok) == 0) {
-			return i;
-		}
-	}
-	if (BTWI(tok[0], 'A', 'Z')) { return SYM_SYMBOL; }
-	if (BTWI(tok[0], 'a', 'z')) { return SYM_SYMBOL; }
-	return sym;
+	if (isNum(tok)) { return SYM_NUMBER; }
+	if (isEnum(tok)) { return SYM_ENUM; }
+
+	if (strcmp(tok, "(") == 0) { return SYM_LPAR; }
+	if (strcmp(tok, ")") == 0) { return SYM_RPAR; }
+	if (strcmp(tok, "if") == 0) { return SYM_IF; }
+	if (strcmp(tok, "true") == 0) { return SYM_TRUE; }
+	if (strcmp(tok, "false") == 0) { return SYM_FALSE; }
+	if (strcmp(tok, "nil") == 0) { return SYM_NIL; }
+
+	message("Unknown symbol", 0);
+	return SYM_UNK;
 }
 
-int nextSym() {
-	sym = symSub();
-	// printf("-sym=%d/%s/%ld-", sym, tok, theNum);
-	return sym;
-}
+void nextSym() { sym = symSub(); }
 
 /*---------------------------------------------------------------------------*/
 /* Parser. */
-enum { ATOM_LIST, ATOM_NUMBER, ATOM_STRING, ATOM_SYMBOL,
+
+enum {
+	ATOM_LIST, ATOM_NUMBER, ATOM_STRING, ATOM_ENUM,
 	ATOM_NIL, ATOM_TRUE, ATOM_FALSE, ATOM_SEQ
 };
 
 NODE_T *buildAtom() {
-	NODE_T *x;
-	// printf("(ba)");
-	if (sym == SYM_EOI) { return NULL; }
-	
-	if (sym == SYM_NUM) {
-		// printf("-num:%ld-", theNum);
-		x = myAlloc(ATOM_NUMBER, NULL);
-		x->val.nVal = theNum;
-		return x;
-	}
+	NODE_T *x = NULL, *n = NULL, *a = NULL;
 
-	if (sym == SYM_SYMBOL) {
-		x = myAlloc(ATOM_SYMBOL, NULL);
-		strcpy(x->val.cVal, tok);
-		return x;
-	}
-
-	if (sym == SYM_T) {
-		x = myAlloc(ATOM_TRUE, NULL);
-		return x;
-	}
-
-	if (sym == SYM_F) {
-		x = myAlloc(ATOM_FALSE, NULL);
-		return x;
-	}
-
-	if (sym == SYM_NIL) {
-		x = myAlloc(ATOM_NIL, NULL);
-		return x;
-	}
-
-	if (sym == SYM_LPAR) {
-		// printf("(list)");
-		x = myAlloc(ATOM_LIST, NULL);
-		x->child = NULL;
-		NODE_T *n, *a;
-		nextSym();
-		while (sym != SYM_RPAR) {
-			// printf("(na)");
-			if (sym == SYM_EOI) { syntax_error(); }
-			a = buildAtom();
-			if (x->child == NULL) { x->child = a; }
-			else { n->next = a; }
-			n = a;
+	switch (sym) {
+		case SYM_UNK: return NULL;
+		case SYM_TRUE: return myAlloc(ATOM_TRUE, NULL);
+		case SYM_FALSE: return myAlloc(ATOM_FALSE, NULL);
+		case SYM_NIL: return myAlloc(ATOM_NIL, NULL);
+		case SYM_NUMBER:
+			x = myAlloc(ATOM_NUMBER, NULL);
+			x->val.nVal = theNum;
+			return x;
+		case SYM_ENUM:
+			x = myAlloc(ATOM_ENUM, NULL);
+			strcpy(x->val.cVal, tok);
+			return x;
+		case SYM_STRING:
+			x = myAlloc(ATOM_STRING, NULL);
+			strcpy(x->val.cVal, tok);
+			return x;
+		case SYM_LPAR:
+			x = myAlloc(ATOM_LIST, NULL);
+			x->child = NULL;
 			nextSym();
-		}
-		return x;
+			while (sym != SYM_RPAR) {
+				if (sym == SYM_UNK) { return NULL; }
+				a = buildAtom();
+				if (x->child == NULL) { x->child = a; }
+				else if (n) { n->next = a; }
+				n = a;
+				nextSym();
+			}
+			return x;
+		default: syntax_error();
 	}
+	
 	return NULL;
 }
 
@@ -183,31 +197,29 @@ void dumpAtom(NODE_T *obj) {
 	NODE_T *o = obj;
 	while (o) {
 		switch (o->type) {
-			case ATOM_LIST:    zType("( "); dumpAtom(o->child); zType(") ");
-			BCASE ATOM_NUMBER: printf("%ld ", o->val.nVal);
-			BCASE ATOM_STRING: zType("<string> ");
-			BCASE ATOM_SYMBOL: printf("%s ", o->val.cVal);
-			BCASE ATOM_NIL:    zType("<nil> ");
-			BCASE ATOM_TRUE:   zType("<true> ");
-			BCASE ATOM_FALSE:  zType("<false> ");
+			case ATOM_LIST:    zType("( "); dumpAtom(o->child); zType(")");
+			BCASE ATOM_NUMBER: printf("%ld", o->val.nVal);
+			BCASE ATOM_STRING: printf("'%s'", o->val.cVal);
+			BCASE ATOM_ENUM: printf("%s", o->val.cVal);
+			BCASE ATOM_NIL:    zType("<nil>");
+			BCASE ATOM_TRUE:   zType("<true>");
+			BCASE ATOM_FALSE:  zType("<false>");
 			break; default: break;
 		}
+		zType(" ");
 		if (o) { o = o->next; }
 	}
 }
 
-void outer(const char *src) {
-	printf("[%d/%s]\n", strlen(src), src);
+void repl(const char *src) {
 	toIn = (char *)src;
-	nextSym();
+	if (toIn == NULL) { toIn = getInput(); }
 
-	NODE_T *atom = buildAtom();
-	while (atom) {
-		if (atom) {
-			dumpAtom(atom);
-			nextSym();
-			atom = buildAtom();
-		}
+	while (1) {
+		nextSym();
+		NODE_T *atom = buildAtom();
+		if (state == 999) { return; }
+		if (atom) { dumpAtom(atom); }
 	}
 }
 
@@ -218,17 +230,7 @@ int main(int argc, char *argv[]) {
 		myFree(&object[i]);
 	}
 	
-	FILE *fp = fopen(boot_fn, "rb");
-	while (state != 999) {
-		if (fp == 0) { zType("\nlsp>"); }
-		if (tib == fgets(tib, sizeof(tib), fp ? fp : stdin)) {
-			tib[strlen(tib)-1] = 0;
-			outer(tib);
-			if (fp) { zType("\n"); }
-		} else {
-			if (fp) { fclose(fp); }
-			fp = NULL;
-		}
-	}
+	inputFp = fopen(boot_fn, "rb");
+	repl(0);
 	return 0;
 }
